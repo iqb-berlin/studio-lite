04d3d00208857dd797f4e131b037f082a8e7e7a1
mechtelm <mechtelqiqb.hu-berlin.de>
2022-06-17 08:49:02 +0200

    Implement i18n
    - for frontend (some labels in home component)
    - for libs/api-dto (error-messages)

---
+++ apps/frontend/src/app/app-translate-loader.ts
@@ -0,0 +1,15 @@
+import { TranslateLoader } from '@ngx-translate/core';
+import { from, merge, Observable } from 'rxjs';
+import { reduce } from 'rxjs/operators';
+
+export class AppTranslateLoader implements TranslateLoader {
+  getTranslation = (lang: string): Observable<Record<string, string>> => merge(
+    from(import(`../assets/i18n/${lang}.json`)),
+    from(import(`../../../../libs/api-dto/src/assets/i18n/${lang}.json`))
+  ).pipe(
+    reduce((
+      merged: Record<string, string>,
+      file: Record<string, string>
+    ): Record<string, string> => ({ ...merged, ...file }), {})
+  );
+}
