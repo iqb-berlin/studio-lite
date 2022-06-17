9b5e7568b01b8a164b19b56567d6b21a393b09f8
mechtelm <mechtelqiqb.hu-berlin.de>
2022-06-17 10:33:00 +0200

    Fix path to api i18n

--- apps/frontend/src/app/app-translate-loader.ts
+++ apps/frontend/src/app/app-translate-loader.ts
@@ -5,7 +5,7 @@ import { reduce } from 'rxjs/operators';
 export class AppTranslateLoader implements TranslateLoader {
   getTranslation = (lang: string): Observable<Record<string, string>> => merge(
     from(import(`../assets/i18n/${lang}.json`)),
-    from(import(`./apps/api/src/assets/i18n/${lang}.json`))
+    from(import(`../../../api/src/assets/i18n/${lang}.json`))
   ).pipe(
     reduce((
       merged: Record<string, string>,
