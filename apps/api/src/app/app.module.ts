f93eaebcdbb0ce56e17f99f81c86faafd097d7fe
mechtelm <mechtelqiqb.hu-berlin.de>
2022-06-22 13:39:06 +0200

    Prepare unit export in api
    Endpoints tested but dummy content

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-
 import { MulterModule } from '@nestjs/platform-express';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
