3be4af32cde8ba17f6fc67827dd0f9ab21efd1c9
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-07-08 11:27:42 +0200

    Add UnitCommentsComponent to workspace
    - Create tab and routing path

---
+++ apps/frontend/src/app/workspace/unit/unit-comments/unit-comments.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'studio-lite-unit-comments',
+  templateUrl: './unit-comments.component.html',
+  styleUrls: ['./unit-comments.component.scss']
+})
+export class UnitCommentsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- apps/frontend/src/app/workspace/workspace.component.ts
+++ apps/frontend/src/app/workspace/workspace.component.ts
@@ -50,7 +50,8 @@ export class WorkspaceComponent implements OnInit, OnDestroy {
     { path: 'metadata', label: 'Eigenschaften' },
     { path: 'editor', label: 'Editor' },
     { path: 'preview', label: 'Vorschau' },
-    { path: 'schemer', label: 'Kodierung' }
+    { path: 'schemer', label: 'Kodierung' },
+    { path: 'comments', label: 'Kommentare' }
   ];
 
   constructor(
