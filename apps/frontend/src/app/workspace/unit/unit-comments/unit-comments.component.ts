7645ad9b2beb286e1beeda7f0cf4e71fb7be0656
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-07-26 16:57:02 +0200

    Refactor subscription in CommentsComponent and UnitCommentsComponent
    - Replace nested delete comment subscription
    - Use combineLatest in UnitCommentsComponent to synchronise two
    subscriptions
    - Replace ngOnChanges with ngOnInit in CommentsComponent, since the
    initialization of the input variables is now synchronized

--- apps/frontend/src/app/workspace/unit/unit-comments/unit-comments.component.ts
+++ apps/frontend/src/app/workspace/unit/unit-comments/unit-comments.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { combineLatest, Subject, takeUntil } from 'rxjs';
 import { AppService } from '../../../app.service';
 import { WorkspaceService } from '../../workspace.service';
 import { BackendService } from '../../../backend.service';
@@ -9,7 +9,7 @@ import { BackendService } from '../../../backend.service';
   templateUrl: './unit-comments.component.html',
   styleUrls: ['./unit-comments.component.scss']
 })
-export class UnitCommentsComponent implements OnInit, OnDestroy {
+export class UnitCommentsComponent implements OnDestroy {
   userName: string = '';
   userId: number = 0;
   unitId: number = 0;
@@ -21,21 +21,18 @@ export class UnitCommentsComponent implements OnInit, OnDestroy {
     public appService: AppService,
     public workspaceService: WorkspaceService,
     private backendService: BackendService
-  ) {}
-
-  ngOnInit(): void {
-    this.workspaceService.selectedUnit$
-      .subscribe(unitId => {
-        this.unitId = unitId;
+  ) {
+    combineLatest([
+      this.workspaceService.selectedUnit$,
+      this.backendService.getMyData()
+    ])
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(result => {
+        this.unitId = result[0];
         this.workspaceId = this.workspaceService.selectedWorkspaceId;
-      });
-    this.backendService.getMyData()
-      .subscribe(myData => {
-        if (myData) {
-          this.userId = this.appService.authData.userId;
-          const displayName = (myData.lastName ? myData.lastName : this.appService.authData.userName) as string;
-          this.userName = myData.firstName ? `${displayName}, ${myData.firstName}` : displayName;
-        }
+        this.userId = this.appService.authData.userId;
+        const displayName = (result[1].lastName ? result[1].lastName : this.appService.authData.userName) as string;
+        this.userName = result[1].firstName ? `${displayName}, ${result[1].firstName}` : displayName;
       });
   }
 
