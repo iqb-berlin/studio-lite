08c3e0409161df925804c1a7e828240b15cdec2d
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-08-01 15:08:56 +0200

    Add entry for SQL script to create review & reviewUnit to db_update.sh

--- database/db_update.sh
+++ database/db_update.sh
@@ -6,3 +6,4 @@ set -e
 #psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file updates/012_update_database.sql
 #psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file updates/013_update_database.sql
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file updates/014_update_database.sql
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file updates/015_update_database.sql
