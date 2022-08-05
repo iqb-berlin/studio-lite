9a6181918f4fbe0ec8f5c1dcc4dd4e41f851824c
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-08-05 12:07:25 +0200

    Add table, controller, entity and service to upload resource packages.
    Resource packages are external libraries such as GeoGebra,
    which are used by some players and editors.
    They are delivered through the Studio.

---
+++ libs/api-dto/src/lib/dto/resource-package/resource-package-dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class ResourcePackageDto {
+  @ApiProperty()
+  id!: number;
+
+  @ApiProperty()
+  name!: string;
+
+  @ApiProperty()
+  @IsString({ each: true })
+  @IsNotEmpty()
+  elements!: string[];
+
+  @ApiProperty()
+  created_at?: Date;
+}
