b528f5b8070e20537f66ed83903fb7dae452aa4a
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-08-11 11:08:25 +0200

    Display resource packages as sortable table

--- apps/frontend/src/app/admin/resource-packages/resource-packages.component.ts
+++ apps/frontend/src/app/admin/resource-packages/resource-packages.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
-import { Observable } from 'rxjs';
+import { Component, Inject, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, switchMap } from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ResourcePackageDto } from '@studio-lite-lib/api-dto';
+import { HttpParams } from '@angular/common/http';
 import { BackendService as ReadBackendService } from '../../backend.service';
 import { BackendService as WriteBackendService } from '../backend.service';
 
@@ -12,39 +12,39 @@ import { BackendService as WriteBackendService } from '../backend.service';
   styleUrls: ['./resource-packages.component.scss']
 })
 export class ResourcePackagesComponent implements OnInit {
-  uploadForm: UntypedFormGroup;
   resourcePackages!: Observable<ResourcePackageDto[]>;
   imageError: string | null = '';
+  selectedResourcePackages: BehaviorSubject<number[]> = new BehaviorSubject<number[]>([]);
 
   constructor(
-    private fb: UntypedFormBuilder,
+    @Inject('SERVER_URL') public serverUrl: string,
     private snackBar: MatSnackBar,
     private writeBackendService: WriteBackendService,
     private readBackendService: ReadBackendService
   ) {
-    this.uploadForm = this.fb.group({});
   }
 
   ngOnInit(): void {
-    setTimeout(() => {
-      this.updateFormFields();
-    });
+    this.updateFormFields();
   }
 
   updateFormFields(): void {
     this.resourcePackages = this.readBackendService.getResourcePackages();
   }
 
-  upload(fileInput: Event) {
-    const fileInputEventTarget = fileInput.target as HTMLInputElement;
-    if (fileInputEventTarget.files && fileInputEventTarget.files[0]) {
-      const formData = new FormData();
-      formData.append('resourcePackage', fileInputEventTarget.files[0]);
-      this.writeBackendService.createResourcePackage(formData).subscribe(r => console.log(r));
-    }
+  delete(id: number) {
+    this.resourcePackages = this.writeBackendService.deleteResourcePackage(id)
+      .pipe(
+        switchMap(() => this.readBackendService.getResourcePackages())
+      );
   }
 
-  delete(id: number) {
-    this.writeBackendService.deleteResourcePackage(id).subscribe(r => console.log(r));
+  deleteSelected() {
+    let queryParams = new HttpParams();
+    this.selectedResourcePackages.value.forEach(id => { queryParams = queryParams.append('id', id); });
+    this.resourcePackages = this.writeBackendService.deleteResourcePackages(queryParams)
+      .pipe(
+        switchMap(() => this.readBackendService.getResourcePackages())
+      );
   }
 }
