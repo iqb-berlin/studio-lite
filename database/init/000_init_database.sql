443962e617f5b0c73d83a53dcd4c0f970295f099
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-08-17 09:01:14 +0200

    Move sql update statements to the initial database setup

--- database/init/000_init_database.sql
+++ database/init/000_init_database.sql
@@ -124,3 +124,56 @@ create table unit_comment
   created_at timestamp with time zone default now(),
   changed_at timestamp with time zone default now()
 );
+
+create table unit_user
+(
+  unit_id           integer not null
+    references unit
+      on delete cascade,
+  user_id                integer not null
+    references "user"
+      on delete cascade,
+  primary key (unit_id, user_id),
+  last_seen_comment_changed_at TIMESTAMP with time zone
+);
+
+create table review
+(
+  id                      serial
+    primary key,
+  workspace_id            integer                                not null
+    references workspace
+      on delete cascade,
+  name                    varchar(100) not null,
+  link                    varchar(100) not null,
+  password                varchar(100),
+  settings                jsonb
+);
+
+alter table review
+  owner to superdb;
+
+create table review_unit
+(
+  unit_id                      integer not null
+    references unit
+      on delete cascade,
+  review_id                      integer not null
+    references review
+      on delete cascade,
+  "order"            integer,
+  primary key (unit_id, review_id)
+);
+
+alter table review_unit
+  owner to superdb;
+
+create table resource_package
+(
+  id         serial not null
+    primary key,
+  elements   text [],
+  name        varchar(100),
+  created_at timestamp with time zone default now()
+);
+
