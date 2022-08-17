0ed61015e16e6dd72e80921e8630b10c48e1ca10
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2022-08-17 09:01:14 +0200

    Move sql update statements to the initial database setup

--- database/db_update.sh
+++ database/db_update.sh
@@ -5,6 +5,6 @@ set -e
 #psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file updates/011_update_database.sql
 #psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file updates/012_update_database.sql
 #psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file updates/013_update_database.sql
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file updates/014_update_database.sql
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file updates/015_update_database.sql
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file updates/016_update_database.sql
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file updates/014_update_database.sql
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file updates/015_update_database.sql
+#psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" --file updates/016_update_database.sql
