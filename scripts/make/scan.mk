74b4f530e4ec3b3d058adb9ad578f6be64eb5be3
svwolter <sven.wolter@iqb.hu-berlin.de>
2022-09-09 11:51:04 +0200

    Migrate to docker compose version 2 plugin

--- scripts/make/scan.mk
+++ scripts/make/scan.mk
@@ -1,20 +1,21 @@
 .PHONY: scan-app scan-db scan-db-change-management scan-backend scan-frontend
 BASE_DIR := $(shell git rev-parse --show-toplevel)
+TRIVY_VERSION := aquasec/trivy:0.29.2
 
-scan-app: scan-db scan-backend scan-frontend	## scans application images for security vulnerabilities
+scan-app: scan-db scan-db-change-management scan-backend scan-frontend	## scans application images for security vulnerabilities
 
 scan-db:	## scans db image for security vulnerabilities
-	cd $(BASE_DIR) && docker build --pull -f $(BASE_DIR)/database/Postgres.Dockerfile --no-cache --rm -t iqbberlin/studio-lite-db:latest -t scm.cms.hu-berlin.de:4567/iqb/studio-lite/iqbberlin/studio-lite-db:latest .
-	docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/Library/Caches:/root/.cache/ aquasec/trivy:0.29.2 image --security-checks vuln --ignore-unfixed --severity CRITICAL iqbberlin/studio-lite-db:latest
+	cd $(BASE_DIR) && docker build --pull -f $(BASE_DIR)/database/Postgres.Dockerfile --no-cache --rm -t iqbberlin/studio-lite-db:scan .
+	docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/Library/Caches:/root/.cache/ $(TRIVY_VERSION) image --security-checks vuln --ignore-unfixed --severity CRITICAL iqbberlin/studio-lite-db:scan
 
 scan-db-change-management:	## scans db image for security vulnerabilities
-	cd $(BASE_DIR) && docker build --pull -f $(BASE_DIR)/database/Liquibase.Dockerfile --no-cache --rm -t iqbberlin/studio-lite-liquibase:latest -t scm.cms.hu-berlin.de:4567/iqb/studio-lite/iqbberlin/studio-lite-liquibase:latest .
-	docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/Library/Caches:/root/.cache/ aquasec/trivy:0.29.2 image --security-checks vuln --ignore-unfixed --severity CRITICAL iqbberlin/studio-lite-liquibase:latest
+	cd $(BASE_DIR) && docker build --pull -f $(BASE_DIR)/database/Liquibase.Dockerfile --no-cache --rm -t iqbberlin/studio-lite-liquibase:scan .
+	docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/Library/Caches:/root/.cache/ $(TRIVY_VERSION) image --security-checks vuln --ignore-unfixed --severity CRITICAL iqbberlin/studio-lite-liquibase:scan
 
 scan-backend:	## scans backend image for security vulnerabilities
-	cd $(BASE_DIR) && docker build --pull -f $(BASE_DIR)/apps/api/Dockerfile --build-arg project=api --target=prod --no-cache --rm -t iqbberlin/studio-lite-backend:latest -t scm.cms.hu-berlin.de:4567/iqb/studio-lite/iqbberlin/studio-lite-backend:latest .
-	docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/Library/Caches:/root/.cache/ aquasec/trivy:0.29.2 image --security-checks vuln --ignore-unfixed --severity CRITICAL iqbberlin/studio-lite-backend:latest
+	cd $(BASE_DIR) && docker build --pull -f $(BASE_DIR)/apps/api/Dockerfile --build-arg project=api --target=prod --no-cache --rm -t iqbberlin/studio-lite-backend:scan .
+	docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/Library/Caches:/root/.cache/ $(TRIVY_VERSION) image --security-checks vuln --ignore-unfixed --severity CRITICAL iqbberlin/studio-lite-backend:scan
 
 scan-frontend:	## scans frontend image for security vulnerabilities
-	cd $(BASE_DIR) && docker build --pull -f $(BASE_DIR)/apps/frontend/Dockerfile --build-arg project=frontend --target=prod --no-cache --rm -t iqbberlin/studio-lite-frontend:latest -t scm.cms.hu-berlin.de:4567/iqb/studio-lite/iqbberlin/studio-lite-frontend:latest .
-	docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/Library/Caches:/root/.cache/ aquasec/trivy:0.29.2 image --security-checks vuln --ignore-unfixed --severity CRITICAL iqbberlin/studio-lite-frontend:latest
+	cd $(BASE_DIR) && docker build --pull -f $(BASE_DIR)/apps/frontend/Dockerfile --build-arg project=frontend --target=prod --no-cache --rm -t iqbberlin/studio-lite-frontend:scan .
+	docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v ${HOME}/Library/Caches:/root/.cache/ $(TRIVY_VERSION) image --security-checks vuln --ignore-unfixed --severity CRITICAL iqbberlin/studio-lite-frontend:scan
--- scripts/make/test.mk
+++ scripts/make/test.mk
@@ -14,68 +14,68 @@ test-frontend: ## Run frontend tests (only in combination with 'make dev-run')
 	docker exec -it studio-lite-frontend bash -c "nx test frontend"
 
 test-e2e-build: ## Build docker e2e test image (e.g. at nx workspace updates)
-	cd $(BASE_DIR) && docker build --pull -f $(BASE_DIR)/apps/frontend-e2e/Dockerfile -t iqbberlin/studio-lite-frontend-e2e:latest .
+	cd $(BASE_DIR) && docker build --pull -f $(BASE_DIR)/apps/frontend-e2e/Dockerfile -t scm.cms.hu-berlin.de:4567/iqb/studio-lite/iqbberlin/studio-lite-frontend-e2e:latest -t iqbberlin/studio-lite-frontend-e2e:latest .
 
 test-e2e-dev: ## Run all e2e tests in dev environment (only in combination with 'make dev-run')
-	docker-compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e
-	docker rm studio-lite-frontend-e2e
-
-test-e2e-prod: ## Run all e2e tests in production environment (only in combination with 'make production-ramp-up')
-	docker-compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e
+	docker compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e
 	docker rm studio-lite-frontend-e2e
 
 test-e2e-api-dev: ## Run all e2e api tests in dev environment (only in combination with 'make dev-run')
-	docker-compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-api
-	docker rm studio-lite-frontend-e2e
-
-test-e2e-api-prod: ## Run all e2e api tests in production environment (only in combination with 'make production-ramp-up')
-	docker-compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-api
+	docker compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-api
 	docker rm studio-lite-frontend-e2e
 
 test-e2e-ui-chrome-dev: ## Run all e2e ui tests with chrome browser in dev environment (only in combination with 'make dev-run')
-	docker-compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-chrome
-	docker rm studio-lite-frontend-e2e
-
-test-e2e-ui-chrome-prod: ## Run all e2e ui tests with chrome browser in production environment (only in combination with 'make production-ramp-up')
-	docker-compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-chrome
+	docker compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-chrome
 	docker rm studio-lite-frontend-e2e
 
 test-e2e-ui-chrome-mobile-dev: ## Run all e2e ui tests with chrome browser for mobiles in dev environment (only in combination with 'make dev-run')
-	docker-compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-chrome-mobile
-	docker rm studio-lite-frontend-e2e
-
-test-e2e-ui-chrome-mobile-prod: ## Run all e2e ui tests with chrome browser for mobiles in production environment (only in combination with 'make production-ramp-up')
-	docker-compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-chrome-mobile
+	docker compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-chrome-mobile
 	docker rm studio-lite-frontend-e2e
 
 test-e2e-ui-firefox-dev: ## Run all e2e ui tests with firefox browser in dev environment (only in combination with 'make dev-run')
-	docker-compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-firefox
-	docker rm studio-lite-frontend-e2e
-
-test-e2e-ui-firefox-prod: ## Run all e2e ui tests with firefox browser in production environment (only in combination with 'make production-ramp-up')
-	docker-compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-firefox
+	docker compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-firefox
 	docker rm studio-lite-frontend-e2e
 
 test-e2e-ui-firefox-mobile-dev: ## Run all e2e ui tests with firefox browser for mobiles in dev environment (only in combination with 'make dev-run')
-	docker-compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-firefox-mobile
-	docker rm studio-lite-frontend-e2e
-
-test-e2e-ui-firefox-mobile-prod: ## Run all e2e ui tests with firefox browser for mobiles in production environment (only in combination with 'make production-ramp-up')
-	docker-compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-firefox-mobile
+	docker compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-firefox-mobile
 	docker rm studio-lite-frontend-e2e
 
 test-e2e-ui-edge-dev: ## Run all e2e ui tests with edge browser in dev environment (only in combination with 'make dev-run')
-	docker-compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-edge
-	docker rm studio-lite-frontend-e2e
-
-test-e2e-ui-edge-prod: ## Run all e2e ui tests with edge browser in production environment (only in combination with 'make production-ramp-up')
-	docker-compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-edge
+	docker compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-edge
 	docker rm studio-lite-frontend-e2e
 
 test-e2e-ui-edge-mobile-dev: ## Run all e2e ui tests with edge browser for mobiles in dev environment (only in combination with 'make dev-run')
-	docker-compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-edge-mobile
+	docker compose --env-file $(BASE_DIR)/.env.dev -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-edge-mobile
+	docker rm studio-lite-frontend-e2e
+
+test-e2e-prod: ## Run all e2e tests in production environment (only in combination with 'make production-ramp-up')
+	docker compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e
+	docker rm studio-lite-frontend-e2e
+
+test-e2e-api-prod: ## Run all e2e api tests in production environment (only in combination with 'make production-ramp-up')
+	docker compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-api
+	docker rm studio-lite-frontend-e2e
+
+test-e2e-ui-chrome-prod: ## Run all e2e ui tests with chrome browser in production environment (only in combination with 'make production-ramp-up')
+	docker compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-chrome
+	docker rm studio-lite-frontend-e2e
+
+test-e2e-ui-chrome-mobile-prod: ## Run all e2e ui tests with chrome browser for mobiles in production environment (only in combination with 'make production-ramp-up')
+	docker compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-chrome-mobile
+	docker rm studio-lite-frontend-e2e
+
+test-e2e-ui-firefox-prod: ## Run all e2e ui tests with firefox browser in production environment (only in combination with 'make production-ramp-up')
+	docker compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-firefox
+	docker rm studio-lite-frontend-e2e
+
+test-e2e-ui-firefox-mobile-prod: ## Run all e2e ui tests with firefox browser for mobiles in production environment (only in combination with 'make production-ramp-up')
+	docker compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-firefox-mobile
+	docker rm studio-lite-frontend-e2e
+
+test-e2e-ui-edge-prod: ## Run all e2e ui tests with edge browser in production environment (only in combination with 'make production-ramp-up')
+	docker compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-edge
 	docker rm studio-lite-frontend-e2e
 
 test-e2e-ui-edge-mobile-prod: ## Run all e2e ui tests with edge browser for mobiles in production environment (only in combination with 'make production-ramp-up')
-	docker-compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-edge-mobile
+	docker compose --env-file $(BASE_DIR)/.env.prod -f $(BASE_DIR)/docker-compose.e2e.yml up test-e2e-ui-edge-mobile
 	docker rm studio-lite-frontend-e2e
