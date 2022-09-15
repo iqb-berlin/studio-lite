10889289403dc081738ccd208b4e593acd6c449c
mechtelm <mechtelqiqb.hu-berlin.de>
2022-09-15 13:04:43 +0200

    Check app version to avoid collision
    * send app version from frontend to backend as part of every request header (interceptor)
    * guard some endpoints and send 520 http exception

---
+++ apps/api/src/app/app-version.guard.ts
@@ -0,0 +1,29 @@
+import {
+  CanActivate, ExecutionContext, HttpException, Inject, Injectable
+} from '@nestjs/common';
+
+@Injectable()
+export class AppVersionGuard implements CanActivate {
+  constructor(
+    @Inject('APP_VERSION') readonly appVersion: string
+  ) {}
+
+  async canActivate(
+    context: ExecutionContext
+  ) {
+    const req = context.switchToHttp().getRequest();
+    const headerAppVersion = req.headers['app-version'];
+    const canAccess = headerAppVersion === this.appVersion;
+    if (!canAccess) {
+      throw new HttpException(
+        `Unexpected app version: ${headerAppVersion} - must be ${this.appVersion}`, 520
+      );
+    }
+    return true;
+  }
+}
+
+export const AppVersionProvider = {
+  provide: 'APP_VERSION',
+  useValue: '2.1.0-rc3'
+};
