948280521cfaa0494511545ed0a189ea1f451f80
mechtelm <mechtel@iqb.hu-berlin.de>
2022-10-13 16:29:24 +0200

    Update angular step 2

--- apps/frontend/src/app/home/about.component.ts
+++ apps/frontend/src/app/home/about.component.ts
@@ -6,9 +6,9 @@ import { AppService } from '../app.service';
     <div class="page-body">
       <div fxLayout="row" fxLayoutAlign="center start">
         <mat-card fxFlex="0 2 500px" class="single-box">
-          <mat-card-title>{{appService.appConfig?.appTitle}} - {{appService.appConfig?.pageTitle}}</mat-card-title>
+          <mat-card-title>{{appService.appConfig.appTitle}} - {{appService.appConfig.pageTitle}}</mat-card-title>
           <mat-card-content>
-            <div [innerHTML]="appService.appConfig?.imprintHtml"></div>
+            <div [innerHTML]="appService.appConfig.imprintHtml"></div>
           </mat-card-content>
           <mat-card-actions>
             <button mat-raised-button [routerLink]="['/']">{{'home.home-page' | translate}}</button>
@@ -18,8 +18,8 @@ import { AppService } from '../app.service';
     </div>
   `,
   styles: ['div.intro-main {margin: 40px; max-width: 600px;}',
-    '.single-box {background: var(--st-box-background)}']
-})
+  '.single-box {background: var(--st-box-background)}']
+  })
 
 export class AboutComponent implements OnInit {
   constructor(
