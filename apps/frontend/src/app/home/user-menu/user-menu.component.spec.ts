1d5b41e25ac309bb35bca18f509081acc22db134
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-01-27 14:43:32 +0100

    [frontend] Fix unit test for UserMenuComponent

--- apps/frontend/src/app/home/user-menu/user-menu.component.spec.ts
+++ apps/frontend/src/app/home/user-menu/user-menu.component.spec.ts
@@ -1,14 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TranslateModule } from '@ngx-translate/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { Component, Input } from '@angular/core';
 import { UserMenuComponent } from './user-menu.component';
 
 describe('UserMenuComponent', () => {
   let component: UserMenuComponent;
   let fixture: ComponentFixture<UserMenuComponent>;
 
+  @Component({ selector: 'studio-lite-user-menu-action', template: '' })
+  class MockStudioLiteUserMenuActionComponent {
+    @Input() type!: string;
+    @Input() iconName!: string;
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UserMenuComponent]
+      declarations: [
+        UserMenuComponent,
+        MockStudioLiteUserMenuActionComponent
+      ],
+      imports: [
+        MatIconModule,
+        MatMenuModule,
+        TranslateModule.forRoot()
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserMenuComponent);
