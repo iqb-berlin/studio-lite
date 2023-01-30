a51375f473ea428e482f01ceaa3dcaa2cdcac9e5
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-01-30 15:54:49 +0100

    [frontend] Fix test of UserWorkspacesComponent

--- apps/frontend/src/app/home/user-workspaces/user-workspaces.component.spec.ts
+++ apps/frontend/src/app/home/user-workspaces/user-workspaces.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TranslateModule } from '@ngx-translate/core';
 import { UserWorkspacesComponent } from './user-workspaces.component';
 
 describe('UserWorkspacesComponent', () => {
@@ -8,7 +8,8 @@ describe('UserWorkspacesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UserWorkspacesComponent]
+      declarations: [UserWorkspacesComponent],
+      imports: [TranslateModule.forRoot()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(UserWorkspacesComponent);
