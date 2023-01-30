ab543292f15342747ed459cb373162798a4fd855
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-01-30 15:56:54 +0100

    [frontend] Remove AppService from UserMenuComponent

--- apps/frontend/src/app/home/user-menu/user-menu.component.ts
+++ apps/frontend/src/app/home/user-menu/user-menu.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { AppService } from '../../app.service';
 
 @Component({
   selector: 'studio-lite-user-menu',
   templateUrl: './user-menu.component.html',
   styleUrls: ['./user-menu.component.scss']
 })
-export class UserMenuComponent {
-  constructor(public appService: AppService) { }
-}
+export class UserMenuComponent {}
