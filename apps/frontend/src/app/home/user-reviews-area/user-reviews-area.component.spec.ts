e168ecc0a394c75945e7ea8f1b1f9d8ff4e27459
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-02-01 13:02:45 +0100

    [frontend] Fix unit test for UserReviewsAreaComponent

--- apps/frontend/src/app/home/user-reviews-area/user-reviews-area.component.spec.ts
+++ apps/frontend/src/app/home/user-reviews-area/user-reviews-area.component.spec.ts
@@ -1,7 +1,11 @@
+// eslint-disable-next-line max-classes-per-file
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
-import { Component, Input } from '@angular/core';
+import {
+  Component, Input, Pipe, PipeTransform
+} from '@angular/core';
 import { UserReviewsAreaComponent } from './user-reviews-area.component';
+import { UserIssue } from '../user-issues/user-issue.interface';
 
 describe('UserReviewsAreaComponent', () => {
   let component: UserReviewsAreaComponent;
@@ -12,11 +16,26 @@ describe('UserReviewsAreaComponent', () => {
     @Input() title!: string;
   }
 
+  @Component({ selector: 'studio-lite-user-issues', template: '' })
+  class MockUserIssuesComponent {
+    @Input() issues!: UserIssue[];
+  }
+
+  @Pipe({ name: 'userIssues' })
+  class MockUserIssuesPipe implements PipeTransform {
+    // eslint-disable-next-line class-methods-use-this
+    transform() {
+      return [];
+    }
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
         UserReviewsAreaComponent,
-        MockAreaTitleComponent
+        MockAreaTitleComponent,
+        MockUserIssuesComponent,
+        MockUserIssuesPipe
       ],
       imports: [
         TranslateModule.forRoot()
