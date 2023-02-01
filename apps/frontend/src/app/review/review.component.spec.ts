daf7e81a4f984f01b1b752afd9e36a3473c543ab
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-02-01 13:33:50 +0100

    [frontend] Fix unit test for ReviewComponent

--- apps/frontend/src/app/review/review.component.spec.ts
+++ apps/frontend/src/app/review/review.component.spec.ts
@@ -1,5 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { environment } from '../../environments/environment';
 import { ReviewComponent } from './review.component';
 
 describe('ReviewComponent', () => {
@@ -8,7 +13,18 @@ describe('ReviewComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ReviewComponent]
+      declarations: [ReviewComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        MatButtonModule,
+        MatDialogModule
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [{
+        provide: 'SERVER_URL',
+        useValue: environment.backendUrl
+      }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ReviewComponent);
