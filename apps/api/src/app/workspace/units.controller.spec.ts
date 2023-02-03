effbead08c7047c439d251f3114ea47b2fbe3f13
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-02-03 13:59:14 +0100

    [api] Fix unit test for UnitsController

--- apps/api/src/app/workspace/units.controller.spec.ts
+++ apps/api/src/app/workspace/units.controller.spec.ts
@@ -1,12 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { createMock } from '@golevelup/ts-jest';
 import { UnitsController } from './units.controller';
+import { AuthService } from '../auth/service/auth.service';
+import { UnitService } from '../database/services/unit.service';
+import { UnitUserService } from '../database/services/unit-user.service';
+import { UnitCommentService } from '../database/services/unit-comment.service';
 
 describe('UnitsController', () => {
   let controller: UnitsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UnitsController]
+      controllers: [UnitsController],
+      providers: [
+        {
+          provide: 'APP_VERSION',
+          useValue: '0.0.0'
+        },
+        {
+          provide: AuthService,
+          useValue: createMock<AuthService>()
+        },
+        {
+          provide: UnitService,
+          useValue: createMock<UnitService>()
+        },
+        {
+          provide: UnitUserService,
+          useValue: createMock<UnitUserService>()
+        },
+        {
+          provide: UnitCommentService,
+          useValue: createMock<UnitCommentService>()
+        }
+      ]
     }).compile();
 
     controller = module.get<UnitsController>(UnitsController);
