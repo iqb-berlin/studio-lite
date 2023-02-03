5f23471dc59418c36565eb4d940f33af4edbc958
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-02-03 12:23:38 +0100

    [api] Fix unit test for SettingService

--- apps/api/src/app/database/services/setting.service.spec.ts
+++ apps/api/src/app/database/services/setting.service.spec.ts
@@ -1,12 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { createMock } from '@golevelup/ts-jest';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { SettingService } from './setting.service';
+import { UsersService } from './users.service';
+import Setting from '../entities/setting.entity';
 
 describe('SettingService', () => {
   let service: SettingService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [SettingService]
+      providers: [
+        SettingService,
+        {
+          provide: UsersService,
+          useValue: createMock<UsersService>()
+        },
+        {
+          provide: getRepositoryToken(Setting),
+          useValue: createMock<Repository<Setting>>()
+        }
+      ]
     }).compile();
 
     service = module.get<SettingService>(SettingService);
