4ea1ef32e7c5acdeb994a6e54e542a51cb10d0e8
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-02-03 13:20:17 +0100

    [api] Fix unit test for WorkspaceUserService

--- apps/api/src/app/database/services/workspace-user.service.spec.ts
+++ apps/api/src/app/database/services/workspace-user.service.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Repository } from 'typeorm';
+import { createMock } from '@golevelup/ts-jest';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { WorkspaceUserService } from './workspace-user.service';
+import { UnitUserService } from './unit-user.service';
+import Workspace from '../entities/workspace.entity';
+import WorkspaceUser from '../entities/workspace-user.entity';
 
 describe('WorkspaceUserService', () => {
   let service: WorkspaceUserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WorkspaceUserService]
+      providers: [
+        WorkspaceUserService,
+        {
+          provide: UnitUserService,
+          useValue: createMock<UnitUserService>()
+        },
+        {
+          provide: getRepositoryToken(Workspace),
+          useValue: createMock<Repository<Workspace>>()
+        },
+        {
+          provide: getRepositoryToken(WorkspaceUser),
+          useValue: createMock<Repository<WorkspaceUser>>()
+        }
+      ]
     }).compile();
 
     service = module.get<WorkspaceUserService>(WorkspaceUserService);
