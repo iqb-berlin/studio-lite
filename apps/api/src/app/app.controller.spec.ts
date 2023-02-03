8b9baec6e0fdec4de663e7021b3a9a948323a428
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-02-03 14:12:59 +0100

    [api] Fix unit test for AppController

--- apps/api/src/app/app.controller.spec.ts
+++ apps/api/src/app/app.controller.spec.ts
@@ -1,19 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
-
+import { createMock } from '@golevelup/ts-jest';
 import { AppController } from './app.controller';
+import { AuthService } from './auth/service/auth.service';
+import { UsersService } from './database/services/users.service';
+import { WorkspaceService } from './database/services/workspace.service';
+import { ReviewService } from './database/services/review.service';
+import { VeronaModulesService } from './database/services/verona-modules.service';
 
 describe('AppController', () => {
-  let app: TestingModule;
+  let controller: AppController;
 
   beforeAll(async () => {
-    app = await Test.createTestingModule({
-      controllers: [AppController]
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [
+        {
+          provide: 'APP_VERSION',
+          useValue: '0.0.0'
+        },
+        {
+          provide: AuthService,
+          useValue: createMock<AuthService>()
+        },
+        {
+          provide: UsersService,
+          useValue: createMock<UsersService>()
+        },
+        {
+          provide: WorkspaceService,
+          useValue: createMock<WorkspaceService>()
+        },
+        {
+          provide: ReviewService,
+          useValue: createMock<ReviewService>()
+        },
+        {
+          provide: VeronaModulesService,
+          useValue: createMock<VeronaModulesService>()
+        }
+      ]
     }).compile();
+
+    controller = module.get<AppController>(AppController);
   });
 
-  describe('getData', () => {
-    it('should return "Welcome to api!"', () => {
-      const appController = app.get<AppController>(AppController);
-    });
+  it('should be defined"', () => {
+    expect(controller).toBeDefined();
   });
 });
