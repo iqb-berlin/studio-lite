8d3c38de8a77a0b4a91068def555a0abf0094420
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-02-03 14:28:50 +0100

    [api] Fix unit test for WorkspaceGroupsController

--- apps/api/src/app/admin/workspaces/workspace-groups.controller.spec.ts
+++ apps/api/src/app/admin/workspaces/workspace-groups.controller.spec.ts
@@ -1,12 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { createMock } from '@golevelup/ts-jest';
 import { WorkspaceGroupsController } from './workspace-groups.controller';
+import { AuthService } from '../../auth/service/auth.service';
+import { WorkspaceService } from '../../database/services/workspace.service';
+import { WorkspaceGroupService } from '../../database/services/workspace-group.service';
+import { UsersService } from '../../database/services/users.service';
 
 describe('WorkspaceGroupsController', () => {
   let controller: WorkspaceGroupsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [WorkspaceGroupsController]
+      controllers: [WorkspaceGroupsController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: createMock<AuthService>()
+        },
+        {
+          provide: WorkspaceService,
+          useValue: createMock<WorkspaceService>()
+        },
+        {
+          provide: WorkspaceGroupService,
+          useValue: createMock<WorkspaceGroupService>()
+        },
+        {
+          provide: UsersService,
+          useValue: createMock<UsersService>()
+        }
+      ]
     }).compile();
 
     controller = module.get<WorkspaceGroupsController>(WorkspaceGroupsController);
