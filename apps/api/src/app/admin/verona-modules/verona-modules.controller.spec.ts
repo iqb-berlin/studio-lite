86c6a1ea06b4121cfb125568fe3788e8b1551024
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-02-03 14:04:19 +0100

    [api] Fix unit test for VeronaModulesController

--- apps/api/src/app/admin/verona-modules/verona-modules.controller.spec.ts
+++ apps/api/src/app/admin/verona-modules/verona-modules.controller.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { createMock } from '@golevelup/ts-jest';
 import { VeronaModulesController } from './verona-modules.controller';
+import { AuthService } from '../../auth/service/auth.service';
+import { VeronaModulesService } from '../../database/services/verona-modules.service';
 
 describe('VeronaModulesController', () => {
   let controller: VeronaModulesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [VeronaModulesController]
+      controllers: [VeronaModulesController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: createMock<AuthService>()
+        },
+        {
+          provide: VeronaModulesService,
+          useValue: createMock<VeronaModulesService>()
+        }
+      ]
     }).compile();
 
     controller = module.get<VeronaModulesController>(VeronaModulesController);
