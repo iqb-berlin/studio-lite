ceda0f9a4e544385c062a09762211ebb9991a49d
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-02-03 14:31:39 +0100

    [api] Fix unit test for WorkspacesController

--- apps/api/src/app/admin/workspaces/workspaces.controller.spec.ts
+++ apps/api/src/app/admin/workspaces/workspaces.controller.spec.ts
@@ -1,12 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { createMock } from '@golevelup/ts-jest';
 import { WorkspacesController } from './workspaces.controller';
+import { AuthService } from '../../auth/service/auth.service';
+import { WorkspaceService } from '../../database/services/workspace.service';
+import { UsersService } from '../../database/services/users.service';
 
 describe('WorkspacesController', () => {
   let controller: WorkspacesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [WorkspacesController]
+      controllers: [WorkspacesController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: createMock<AuthService>()
+        },
+        {
+          provide: WorkspaceService,
+          useValue: createMock<WorkspaceService>()
+        },
+        {
+          provide: UsersService,
+          useValue: createMock<UsersService>()
+        }
+      ]
     }).compile();
 
     controller = module.get<WorkspacesController>(WorkspacesController);
