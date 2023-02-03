0b3905633ab5d72bfa9623a60f2ddfb33a209365
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-02-03 14:35:32 +0100

    [api] Fix unit test for AuthService

--- apps/api/src/app/auth/service/auth.service.spec.ts
+++ apps/api/src/app/auth/service/auth.service.spec.ts
@@ -1,12 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { createMock } from '@golevelup/ts-jest';
+import { JwtService } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
+import { UsersService } from '../../database/services/users.service';
+import { WorkspaceService } from '../../database/services/workspace.service';
+import { ReviewService } from '../../database/services/review.service';
+import { WorkspaceGroupService } from '../../database/services/workspace-group.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService]
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: createMock<UsersService>()
+        },
+        {
+          provide: WorkspaceService,
+          useValue: createMock<WorkspaceService>()
+        },
+        {
+          provide: ReviewService,
+          useValue: createMock<ReviewService>()
+        },
+        {
+          provide: WorkspaceGroupService,
+          useValue: createMock<WorkspaceGroupService>()
+        },
+        {
+          provide: JwtService,
+          useValue: createMock<JwtService>()
+        }
+      ]
     }).compile();
 
     service = module.get<AuthService>(AuthService);
