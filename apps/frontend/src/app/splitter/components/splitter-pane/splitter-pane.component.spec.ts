8701fdd3db630249e7e62760c64a5fafb469703a
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-02-13 10:35:43 +0100

    Add splitter module

---
+++ apps/frontend/src/app/splitter/components/splitter-pane/splitter-pane.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SplitterPaneComponent } from './splitter-pane.component';
+
+describe('SplitterPaneComponent', () => {
+  let component: SplitterPaneComponent;
+  let fixture: ComponentFixture<SplitterPaneComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [SplitterPaneComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SplitterPaneComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ apps/frontend/src/app/splitter/components/splitter-pane/splitter-pane.component.ts
@@ -0,0 +1,44 @@
+import { Component, ElementRef, Input } from '@angular/core';
+
+@Component({
+  selector: 'studio-lite-splitter-pane',
+  templateUrl: './splitter-pane.component.html',
+  styleUrls: ['./splitter-pane.component.scss']
+})
+export class SplitterPaneComponent {
+  size: number = 0;
+  index: number = 0;
+  @Input() initialSize: number | 'auto' = 'auto';
+  @Input() minSize: number = 0;
+  @Input() maxSize!: number;
+
+  constructor(public elementRef: ElementRef) { }
+
+  init(index: number): void {
+    this.index = index;
+    this.elementRef.nativeElement.style.order = index;
+    if (this.initialSize !== 'auto') {
+      this.setSize(this.initialSize);
+      this.setStyle(this.size);
+    }
+  }
+
+  setSize(size: number): void {
+    let paneSize = size < this.minSize ? this.minSize : size;
+    paneSize = this.maxSize && paneSize > this.maxSize ? this.maxSize : paneSize;
+    this.size = paneSize;
+  }
+
+  calculateSize(isBeforeGutter: boolean, position: number, gutterOffset: number): void {
+    const paneSize = isBeforeGutter ?
+      position - this.elementRef.nativeElement.offsetLeft :
+      (this.elementRef.nativeElement.offsetLeft + this.elementRef.nativeElement.offsetWidth) - position - gutterOffset;
+    this.setSize(paneSize);
+  }
+
+  setStyle(paneSize: number): void {
+    this.elementRef.nativeElement.style.flexShrink = 0;
+    this.elementRef.nativeElement.style.flexGrow = 0;
+    this.elementRef.nativeElement.style.flexBasis = `${paneSize}px`;
+  }
+}
