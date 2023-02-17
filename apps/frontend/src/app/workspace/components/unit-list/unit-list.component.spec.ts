3224f5e46e6f696736c8b8e80f4d98ac7628d03c
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-02-17 16:08:05 +0100

    Fix test for UnitListComponent

--- apps/frontend/src/app/workspace/components/unit-list/unit-list.component.spec.ts
+++ apps/frontend/src/app/workspace/components/unit-list/unit-list.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../../../../environments/environment';
 import { UnitListComponent } from './unit-list.component';
 
 describe('UnitListComponent', () => {
@@ -8,7 +10,15 @@ describe('UnitListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UnitListComponent]
+      declarations: [UnitListComponent],
+      imports: [
+        RouterTestingModule,
+        HttpClientModule
+      ],
+      providers: [{
+        provide: 'SERVER_URL',
+        useValue: environment.backendUrl
+      }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(UnitListComponent);
