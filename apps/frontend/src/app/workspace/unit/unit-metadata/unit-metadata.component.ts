8dcc0f92a926cd738c6cb0e0f8a7280f609ae8e2
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-03-27 12:56:28 +0200

    [frontend] Add NewGroupButtonComp. to reduce code of UnitMetadataComp.

--- apps/frontend/src/app/workspace/unit/unit-metadata/unit-metadata.component.ts
+++ apps/frontend/src/app/workspace/unit/unit-metadata/unit-metadata.component.ts
@@ -3,14 +3,8 @@ import {
 } from '@angular/core';
 import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
-import { MatDialog } from '@angular/material/dialog';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { ModuleService, SelectModuleComponent } from '@studio-lite/studio-components';
 import { WorkspaceService } from '../../workspace.service';
-import { BackendService } from '../../backend.service';
-import { BackendService as AppBackendService } from '../../../backend.service';
-import { InputTextComponent } from '../../../components/input-text.component';
-import { AppService } from '../../../app.service';
 
 @Component({
   templateUrl: './unit-metadata.component.html',
@@ -31,13 +25,8 @@ export class UnitMetadataComponent implements OnInit, OnDestroy {
 
   constructor(
     private fb: UntypedFormBuilder,
-    private backendService: BackendService,
-    private appBackendService: AppBackendService,
     public workspaceService: WorkspaceService,
-    public appService: AppService,
-    public moduleService: ModuleService,
-    private inputTextDialog: MatDialog,
-    private snackBar: MatSnackBar
+    public moduleService: ModuleService
   ) {
     this.timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
     this.unitForm = this.fb.group({
@@ -129,39 +118,5 @@ export class UnitMetadataComponent implements OnInit, OnDestroy {
     if (this.editorSelectionChangedSubscription) this.editorSelectionChangedSubscription.unsubscribe();
     if (this.playerSelectionChangedSubscription) this.playerSelectionChangedSubscription.unsubscribe();
     if (this.schemerSelectionChangedSubscription) this.schemerSelectionChangedSubscription.unsubscribe();
-  }
-
-  newGroup() {
-    const dialogRef = this.inputTextDialog.open(InputTextComponent, {
-      width: '500px',
-      data: {
-        title: 'Neue Gruppe',
-        default: '',
-        okButtonLabel: 'Speichern',
-        prompt: 'Name der Gruppe'
-      }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (typeof result === 'string') {
-        if (!this.workspaceService.workspaceSettings.unitGroups) {
-          this.workspaceService.workspaceSettings.unitGroups = [];
-        }
-        if (this.workspaceService.workspaceSettings.unitGroups.indexOf(result) < 0) {
-          this.workspaceService.workspaceSettings.unitGroups.push(result);
-          this.appBackendService.setWorkspaceSettings(
-            this.workspaceService.selectedWorkspaceId, this.workspaceService.workspaceSettings
-          ).subscribe(isOK => {
-            if (!isOK) {
-              this.snackBar.open('Neue Gruppe konnte nicht gespeichert werden.', '', { duration: 3000 });
-            } else {
-              this.snackBar.open('Neue Gruppe gespeichert', '', { duration: 1000 });
-              const groupControl = this.unitForm.get('group');
-              if (groupControl) groupControl.setValue(result);
-            }
-          });
-        }
-      }
-    });
   }
 }
