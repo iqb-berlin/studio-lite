afb52fc4769e4b1a54ecf86539ab2675403e2efd
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-06-06 14:16:36 +0200

    [frontend] Fix paths to template and styles of SettingsComponent

--- apps/frontend/src/app/modules/admin/components/settings/settings.component.ts
+++ apps/frontend/src/app/modules/admin/components/settings/settings.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'studio-lite-settings',
-  templateUrl: 'settings.component.html',
-  styleUrls: ['settings.component.scss']
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.scss']
 })
 export class SettingsComponent {}
