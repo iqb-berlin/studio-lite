3fead01d33a98aa59d3fd9a6d1b8f4abd4552c78
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-07-07 12:19:43 +0200

    [frontend] Refactor ReviewComponent
    - Move to own folder
    - Add test file
    - Split code into subcomponents (UnitNavCom., AddCommentButtonComp.)
    - Replace Flex layout module with css classes
    - Add i18n translations

---
+++ apps/frontend/src/app/modules/review/components/add-comment-button/add-comment-button.component.spec.ts
@@ -0,0 +1,31 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { AddCommentButtonComponent } from './add-comment-button.component';
+
+describe('AddCommentButtonComponent', () => {
+  let component: AddCommentButtonComponent;
+  let fixture: ComponentFixture<AddCommentButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AddCommentButtonComponent],
+      imports: [
+        TranslateModule.forRoot(),
+        MatIconModule,
+        MatTooltipModule,
+        MatDialogModule
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AddCommentButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
