3148df815a8a23a78d715176c04ac9c78a19b9b0
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-07-10 13:53:50 +0200

    [frontend] Move UnitInfoCommentsComponent to own folder
    - Add styles, test and template files
    - Rename selector
    - Add i18n translations
    - Replace Flex layout module with css classes
    - Rename selector of UnitInfoLoader

--- apps/frontend/src/app/modules/review/components/unit-info-comments/unit-info-comments.component.ts
+++ apps/frontend/src/app/modules/review/components/unit-info-comments/unit-info-comments.component.ts
@@ -4,51 +4,20 @@ import {
 import { BackendService } from '../../services/backend.service';
 import { ReviewService } from '../../services/review.service';
 import { Comment } from '../../../comments/models/comment.interface';
-import { UnitInfoLoaderComponent } from './unit-info-loader.component';
+import { UnitInfoLoaderComponent } from '../../units/unit-info/unit-info-loader.component';
 
 @Component({
-  selector: 'unit-info-comments',
-  template: `
-    <div fxLayout="column" [style.minHeight.px]="minHeight">
-      <div class="unit-info-comment-header">Kommentare</div>
-      <unit-info-loader (onEnter)="updateContent()"></unit-info-loader>
-      <div *ngIf="allComments.length === 0">{{ loader?.spinnerOn ? 'lade...' : 'Keine Kommentare verfügbar.'}}</div>
-      <div class="unit-info-comment-content" *ngFor="let c of allComments">
-        <div fxLayout="row" fxLayoutAlign="space-between">
-            <h4><mat-icon *ngIf="c.parentId">subdirectory_arrow_right</mat-icon>{{c.userName}}</h4>
-            <p>{{c.changedAt | date : 'dd.MM.yyyy' : timeZone}}</p>
-        </div>
-        <div [innerHTML]="c.body" [class]="c.parentId ? 'reply-comment' : ''"></div>
-      </div>
-    </div>
-  `,
-  styles: [
-    `.unit-info-comment-header {
-      align-content: stretch;
-      background-color: rgba(87,247,147,0.32);
-      font-size: large;
-      padding: 2px 6px;
-    }`,
-    `.unit-info-comment-content {
-      border-top: 1px solid darkgray;
-    }`,
-    `.reply-comment {
-      margin-left: 20px;
-    }`,
-    `.unit-info-comment-content h4 {
-      margin-top: 2px;
-    }`
-  ]
+  selector: 'studio-lite-unit-info-comments',
+  templateUrl: './unit-info-comments.component.html',
+  styleUrls: ['./unit-info-comments.component.scss']
 })
 export class UnitInfoCommentsComponent {
   @ViewChild(UnitInfoLoaderComponent) loader?: UnitInfoLoaderComponent;
   timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  minHeight = 1000;
   _unitId = 0;
   @Input('unitId')
   set unitId(value: number) {
     this._unitId = value;
-    this.minHeight = 1000;
     this.updateContent();
   }
 
@@ -65,7 +34,6 @@ export class UnitInfoCommentsComponent {
       const unitData = this.reviewService.units[this._unitId];
       if (unitData.comments && unitData.comments.length > 0) {
         this.allComments = unitData.comments;
-        this.minHeight = 0;
       } else {
         this.loader.spinnerOn = true;
         this.backendService.getUnitComments(
@@ -74,7 +42,6 @@ export class UnitInfoCommentsComponent {
           if (this.loader) this.loader.spinnerOn = false;
           unitData.comments = UnitInfoCommentsComponent.sortComments(unitComments);
           this.allComments = unitData.comments;
-          this.minHeight = 0;
         });
       }
     }
