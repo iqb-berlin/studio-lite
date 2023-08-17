35f9795c2d75412a48735f6cc7ea992bce541ff2
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-08-17 11:08:59 +0200

    Remove unused import TranslateModule

--- apps/frontend/src/app/modules/workspace/components/unit-groups/unit-groups.component.spec.ts
+++ apps/frontend/src/app/modules/workspace/components/unit-groups/unit-groups.component.spec.ts
@@ -1,6 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { TranslateModule } from '@ngx-translate/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { environment } from '../../../../../environments/environment';
