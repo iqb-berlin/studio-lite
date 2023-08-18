4d6c1f1db437b773bd68fc6cc6dad74b1dbe7f83
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-08-18 10:22:21 +0200

    Remove not needed imports and injections

--- apps/frontend/src/app/modules/workspace/components/unit-groups/unit-groups.component.ts
+++ apps/frontend/src/app/modules/workspace/components/unit-groups/unit-groups.component.ts
@@ -1,26 +1,13 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { UnitInListDto } from '@studio-lite-lib/api-dto';
-import { ActivatedRoute, Router } from '@angular/router';
-import { SelectUnitDirective } from '../../directives/select-unit.directive';
-import { BackendService } from '../../services/backend.service';
-import { WorkspaceService } from '../../services/workspace.service';
+import {
+  Component, Input
+} from '@angular/core';
 
 @Component({
   selector: 'studio-lite-unit-groups',
   templateUrl: './unit-groups.component.html',
   styleUrls: ['./unit-groups.component.scss']
 })
-export class UnitGroupsComponent extends SelectUnitDirective {
-  @Input() selectedUnitId!: number;
+export class UnitGroupsComponent {
   @Input() filter!: string;
   @Input() expanded!: boolean;
-  unitList!: { [p: string]: UnitInListDto[] };
-  constructor(
-    public workspaceService: WorkspaceService,
-    public router: Router,
-    public route: ActivatedRoute,
-    public backendService: BackendService
-  ) {
-    super();
-  }
 }
