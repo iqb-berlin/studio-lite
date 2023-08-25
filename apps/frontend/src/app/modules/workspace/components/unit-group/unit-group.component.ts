5d5e0dfdf19fbf5573da7e8934ef6e00377fc08e
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-08-25 09:24:11 +0200

    [frontend] Fix unsubscription in unit group

--- apps/frontend/src/app/modules/workspace/components/unit-group/unit-group.component.ts
+++ apps/frontend/src/app/modules/workspace/components/unit-group/unit-group.component.ts
@@ -1,7 +1,7 @@
 import {
   Component, EventEmitter, Input, OnInit, Output
 } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'studio-lite-unit-group',
@@ -14,14 +14,16 @@ export class UnitGroupComponent implements OnInit {
   @Input() count!: number;
   @Input() expandAll!: BehaviorSubject<boolean>;
   @Output() expandedChange = new EventEmitter<boolean>();
+  private ngUnsubscribe = new Subject<void>();
 
   ngOnInit(): void {
-    this.expandAll.subscribe(expanded => {
+    this.expandAll.pipe(takeUntil(this.ngUnsubscribe)).subscribe(expanded => {
       this.expanded = expanded;
     });
   }
 
   ngOnDestroy(): void {
-    if (this.expandAll) this.expandAll.unsubscribe();
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
