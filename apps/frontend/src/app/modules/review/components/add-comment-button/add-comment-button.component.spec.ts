f90118884f562e5b6f0ff71831f889a4281edbdc
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-08-29 14:16:09 +0200

    [frontend] Provide review service in add comment button test

--- apps/frontend/src/app/modules/review/components/add-comment-button/add-comment-button.component.spec.ts
+++ apps/frontend/src/app/modules/review/components/add-comment-button/add-comment-button.component.spec.ts
@@ -3,8 +3,10 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { createMock } from '@golevelup/ts-jest';
 import { AddCommentButtonComponent } from './add-comment-button.component';
 import { WrappedIconComponent } from '../../../shared/components/wrapped-icon/wrapped-icon.component';
+import { ReviewService } from '../../services/review.service';
 
 describe('AddCommentButtonComponent', () => {
   let component: AddCommentButtonComponent;
@@ -13,6 +15,10 @@ describe('AddCommentButtonComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [AddCommentButtonComponent, WrappedIconComponent],
+      providers: [{
+        provide: ReviewService,
+        useValue: createMock<ReviewService>()
+      }],
       imports: [
         TranslateModule.forRoot(),
         MatIconModule,
