a1f6c8a9a913f5434e12f02e18db899cf7581f04
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-09-06 07:46:46 +0200

    Create move workspace dialog component

--- apps/frontend/src/app/modules/shared/components/move-workspace/move-workspace.component.ts
+++ apps/frontend/src/app/modules/shared/components/move-workspace/move-workspace.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
+export interface MoveComponentData {
+  title: string,
+  prompt: string,
+  default: string,
+  okButtonLabel: string,
+  wResponse: number[]
+}
 @Component({
   selector: 'studio-lite-move-workspace',
   templateUrl: './move-workspace.component.html',
   styleUrls: ['./move-workspace.component.scss']
 })
-export class MoveWorkspaceComponent {}
+export class MoveWorkspaceComponent {
+  typedData: MoveComponentData;
+  selectedValue:string = '';
+  constructor(@Inject(MAT_DIALOG_DATA) data: unknown) {
+    this.typedData = data as MoveComponentData;
+  }
+}
