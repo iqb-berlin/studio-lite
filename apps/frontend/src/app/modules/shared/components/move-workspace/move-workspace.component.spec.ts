0819f5500f9c512f7067e20969eb9efcac7ba6d7
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-09-08 10:50:49 +0200

    [frontend] Fix unit test move-workspace component

--- apps/frontend/src/app/modules/shared/components/move-workspace/move-workspace.component.spec.ts
+++ apps/frontend/src/app/modules/shared/components/move-workspace/move-workspace.component.spec.ts
@@ -1,5 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MoveWorkspaceComponent } from './move-workspace.component';
+import {MAT_DIALOG_DATA, MatDialogModule} from "@angular/material/dialog";
+import {MatFormFieldControl, MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {ReactiveFormsModule} from "@angular/forms";
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {TranslateModule} from "@ngx-translate/core";
+import {MatSelect, MatSelectModule} from "@angular/material/select";
 
 describe('MoveWorkspaceComponent', () => {
   let component: MoveWorkspaceComponent;
@@ -7,7 +14,21 @@ describe('MoveWorkspaceComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [MoveWorkspaceComponent]
+      declarations: [MoveWorkspaceComponent],
+      imports: [
+        TranslateModule.forRoot(),
+        MatDialogModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatSelectModule,
+        NoopAnimationsModule
+      ],
+      providers: [
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {}
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(MoveWorkspaceComponent);
