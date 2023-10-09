9b0a26f4987a9d515008769be251feeaaeea6e60
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-10-09 11:19:38 +0200

    Adding nested tree component in shared module

---
+++ apps/frontend/src/app/modules/shared/components/nested-tree/nested-tree.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { NestedTreeComponent } from './nested-tree.component';
+import { WrappedIconComponent } from '../wrapped-icon/wrapped-icon.component';
+
+describe('NestedTreeComponent', () => {
+  let component: NestedTreeComponent;
+  let fixture: ComponentFixture<NestedTreeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NestedTreeComponent, WrappedIconComponent],
+      imports: [TranslateModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NestedTreeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
