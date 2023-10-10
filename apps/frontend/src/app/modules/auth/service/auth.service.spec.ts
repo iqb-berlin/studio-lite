71c108248d7a2b293e3b11f832c60d3206d1652c
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-10-10 13:55:09 +0200

    Fix unit tests

--- apps/frontend/src/app/modules/auth/service/auth.service.spec.ts
+++ apps/frontend/src/app/modules/auth/service/auth.service.spec.ts
@@ -1,11 +1,24 @@
 import { TestBed } from '@angular/core/testing';
+import { TranslateModule } from '@ngx-translate/core';
+import { HttpClientModule } from '@angular/common/http';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        TranslateModule.forRoot(),
+        HttpClientModule
+      ],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: true
+        }
+      ]
+    });
     service = TestBed.inject(AuthService);
   });
 
