f8d127ca435853c112614c044540e728de6f554e
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-10-12 17:22:28 +0200

    [frontend] Add nested-tree-node component to shared module

---
+++ apps/frontend/src/app/modules/shared/components/nested-tree-node/nested-tree-node.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, signal } from '@angular/core';
+
+@Component({
+  selector: 'studio-lite-nested-tree-node',
+  templateUrl: './nested-tree-node.component.html',
+  styleUrls: ['./nested-tree-node.component.scss']
+
+})
+export class NestedTreeNodeComponent {
+  checkboxSelected = signal<boolean>(false);
+  @Input() node = {
+    url: '', name: '', description: '', notation: ''
+  };
+
+  @Input() values:Array<string> = [];
+  onSelect() {
+    this.checkboxSelected.set(!this.checkboxSelected());
+    if (this.checkboxSelected()) {
+      this.values.push(this.node.notation);
+    } else {
+      this.values.filter((value, index) => {
+        if (value === this.node.notation) {
+          // Removes the value from the original array
+          this.values.splice(index, 1);
+          return true;
+        }
+        return false;
+      });
+    }
+  }
+}
