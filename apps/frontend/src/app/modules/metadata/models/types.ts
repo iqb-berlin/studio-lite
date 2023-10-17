488f5d415d6b699ad3b8f7cdf607915d4800e050
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-10-17 16:42:39 +0200

    [frontend] Adding type definitions in metadata component

---
+++ apps/frontend/src/app/modules/metadata/models/types.ts
@@ -0,0 +1,38 @@
+export type SelectedNode = {
+  id: string,
+  notation:string,
+  label:string,
+  description:string
+};
+
+export type NotationNode = {
+  id: string,
+  notation:string,
+  description:string,
+  label?:string,
+  name:string,
+  children?: NotationNode[] | [],
+  url?:string,
+  prefLabel?:{
+    de:string
+  },
+  selected:boolean,
+  narrower?: NotationNode[]
+};
+
+export type NestedTreeParameters = {
+  url: string,
+  allowMultipleValues: boolean,
+  maxLevel: number,
+  hideNumbering: boolean,
+  hideDescription: boolean,
+  hideTitle: boolean,
+  addTextLanguages: Array<string>
+};
+
+export type DialogData = {
+  vocab: any,
+  value: { name:string },
+  selectedNodes: SelectedNode[],
+  params: NestedTreeParameters
+};
