66fb3069a1a8be3bd4bab7ea9efa345e8492e493
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-10-19 17:36:46 +0200

    Use trueLabel and falseLabel for new FormlyToggleComponent
    - Add labels dictionary

--- apps/frontend/src/app/modules/metadata/components/metadata/metadata.component.ts
+++ apps/frontend/src/app/modules/metadata/components/metadata/metadata.component.ts
@@ -13,6 +13,9 @@ import * as profile from './profile.json';
 export class MetadataComponent implements OnInit {
   @Output() modelChange: EventEmitter<any> = new EventEmitter<any>();
   @Input() model!: any;
+
+  labels: Record<string, string>[] = [];
+
   form = new FormGroup({});
   profile!: any;
   groups!:any;
@@ -26,40 +29,57 @@ export class MetadataComponent implements OnInit {
     this.fields = this.jsonToFormlyMapper();
   }
 
+  addLabel(entry: any, labelKey: string): Record<string, string> | null {
+    const labelsLength = entry[labelKey].length;
+    if (labelsLength) {
+      if (labelKey === 'label') {
+        this.labels[entry.id] = entry.label[0]?.value;
+      }
+      return {
+        [`props.${labelKey}`]:
+          `field.model.lang !== "de" ? "${entry[labelKey][0].value}" : "${entry[labelKey][labelsLength - 1]?.value}"`
+      };
+    }
+    return null;
+  }
+
   private jsonToFormlyMapper() {
     const groups = this.profile?.groups;
 
     const typesMapping: any = {
       text: 'input',
-      boolean: 'toggle',
+      boolean: 'formlyToggle',
       number: 'number',
       vocabulary: 'chips',
       textarea: 'textarea'
     };
-
     return groups?.map((group: { label: { value: any; }[]; entries: any[]; }) => ({
       key: 'text',
       wrappers: ['panel'],
       props: { label: group.label[0].value },
       fieldGroup:
-          group.entries.map(entry => {
-            const props = {
-              placeholder: '',
-              ...entry.parameters
-            };
-            if (entry.type === 'text' && entry.parameters?.multiLine) {
-              entry.type = 'textarea';
-            }
-            return (
-              {
-                key: entry.id,
-                type: typesMapping[entry.type],
-                props: props,
-                expressions: {
-                  'props.label': `field.model.lang !== "de" ? "${entry.label[0]?.value}" : "${entry.label[1]?.value}"`
-                }
-              });
-          })
+        group.entries.map(entry => {
+          const props = {
+            placeholder: '',
+            ...entry.parameters
+          };
+          if (entry.type === 'text' && entry.parameters?.multiLine) {
+            entry.type = 'textarea';
+          }
+          return (
+            {
+              key: entry.id,
+              type: typesMapping[entry.type],
+              props: props,
+              expressions: {
+                ...(entry.label && entry.label.length && this.addLabel(entry, 'label')),
+                ...(entry.parameters && entry.parameters.trueLabel &&
+                  entry.parameters.trueLabel.length && this.addLabel(entry.parameters, 'trueLabel')),
+                ...(entry.parameters && entry.parameters.falseLabel &&
+                  entry.parameters.falseLabel.length && this.addLabel(entry.parameters, 'falseLabel'))
+              }
+            });
+        })
     })
     );
   }
