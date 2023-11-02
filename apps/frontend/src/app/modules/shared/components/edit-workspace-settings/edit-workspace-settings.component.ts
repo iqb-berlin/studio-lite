15873e6ce32ffe192e083f2bd1ebdf92de6b4f37
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-11-02 15:59:16 +0100

    Remove states configuration in workspace settings

--- apps/frontend/src/app/modules/shared/components/edit-workspace-settings/edit-workspace-settings.component.ts
+++ apps/frontend/src/app/modules/shared/components/edit-workspace-settings/edit-workspace-settings.component.ts
@@ -64,47 +64,6 @@ export class EditWorkspaceSettingsComponent implements OnInit {
     this.dialogData.itemMDProfile = e.value;
   }
 
-  addState() {
-    if (this.selectionChanged) {
-      this.selectionChanged.push({
-        id: this.selectionChanged.length + 1,
-        label: '',
-        color: ''
-      });
-      this.dialogData.states = this.selectionChanged;
-    } else {
-      this.selectionChanged = [{
-        id: 1,
-        label: '',
-        color: '#edb211'
-      }];
-      this.dialogData.states = this.selectionChanged;
-    }
-  }
-
-  stateSelectionChange(e:any) {
-    const foundState = this.dialogData.states?.find(state => state.id === parseInt(e.target.id, 10));
-    if (foundState) {
-      this.selectionChanged = this.selectionChanged.map(state => {
-        if (state.id === foundState.id) {
-          if (e.target.type === 'color') {
-            return { ...state, color: e.target?.value };
-          }
-          if (e.target.type === 'text') {
-            return { ...state, label: e.target?.value };
-          }
-        }
-        return state;
-      });
-    }
-  }
-
-  deleteState(id:number) {
-    this.dialogData.states = this.dialogData?.states
-      ?.filter(state => state.id !== id).map((state, index) => ({ ...state, id: index + 1 }));
-    this.selectionChanged = this.dialogData.states as State[];
-  }
-
   addData() {
     this.dialogData.states = this.selectionChanged;
     this.selectionChanged = this.dialogData.states as State[];
