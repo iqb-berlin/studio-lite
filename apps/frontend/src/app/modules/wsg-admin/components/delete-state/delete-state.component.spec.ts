b0e0c6e34a5a707316d3ba3284c9951f736cb6f6
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-11-02 15:57:24 +0100

    Add delete state dialog component

---
+++ apps/frontend/src/app/modules/wsg-admin/components/delete-state/delete-state.component.spec.ts
@@ -0,0 +1,62 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatDialogModule } from '@angular/material/dialog';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { TranslateModule } from '@ngx-translate/core';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTableModule } from '@angular/material/table';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { Component, Input } from '@angular/core';
+import { environment } from '../../../../../environments/environment';
+import { DeleteStateComponent } from './delete-state.component';
+import { WrappedIconComponent } from '../../../shared/components/wrapped-icon/wrapped-icon.component';
+
+describe('UsersComponent', () => {
+  let component: DeleteStateComponent;
+  let fixture: ComponentFixture<DeleteStateComponent>;
+
+  @Component({ selector: 'studio-lite-search-filter', template: '' })
+  class MockSearchFilterComponent {
+    @Input() title!: string;
+  }
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        DeleteStateComponent,
+        MockSearchFilterComponent,
+        WrappedIconComponent
+      ],
+      imports: [
+        MatDialogModule,
+        MatTableModule,
+        MatCheckboxModule,
+        HttpClientModule,
+        MatSnackBarModule,
+        MatListModule,
+        MatIconModule,
+        MatTooltipModule,
+        TranslateModule.forRoot()
+      ],
+      providers: [
+        {
+          provide: 'SERVER_URL',
+          useValue: environment.backendUrl
+        }
+      ]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DeleteStateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
