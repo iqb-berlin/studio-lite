dd4fcd12bcd1ec5326ca6a2e137a0e802589551e
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-11-08 10:00:48 +0100

    Set min and max values for formly duration

--- apps/frontend/src/app/modules/metadata/components/formly-duration/formly-duration.component.ts
+++ apps/frontend/src/app/modules/metadata/components/formly-duration/formly-duration.component.ts
@@ -1,20 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { FieldType } from '@ngx-formly/material';
-import { FieldTypeConfig } from '@ngx-formly/core';
+import { FieldTypeConfig, FormlyFieldProps } from '@ngx-formly/core/public_api';
 
 interface Duration {
   minutes: string;
   seconds: string;
 }
+
+interface FormlyDurationProps extends FormlyFieldProps {
+  minValue?: number;
+  maxValue?: number;
+}
+
 @Component({
   selector: 'studio-lite-formly-duration',
   templateUrl: './formly-duration.component.html',
   styleUrls: ['./formly-duration.component.scss']
 })
-export class FormlyDurationComponent extends FieldType<FieldTypeConfig> implements OnInit {
+export class FormlyDurationComponent extends FieldType<FieldTypeConfig<FormlyDurationProps>> implements OnInit {
   duration: Duration = { minutes: '0', seconds: '0' };
+  minSeconds = 0;
+  maxSeconds!: number;
+  minMinutes = 0;
+  maxMinutes!: number;
   ngOnInit(): void {
     this.convertSecondsToMinutes(this.formControl.value || 0);
+    this.setMinMaxValues();
+  }
+
+  private setMinMaxValues(): void {
+    const minValue = Number(this.props.minValue) || 0;
+    if (minValue < 60) {
+      this.minSeconds = minValue;
+      this.minMinutes = 0;
+    } else {
+      this.minSeconds = 0;
+      this.minMinutes = Math.floor(minValue / 60);
+    }
+    const maxValue = Number(this.props.maxValue) || 0;
+    if (maxValue && maxValue < 60) {
+      this.maxSeconds = maxValue;
+      this.maxMinutes = 0;
+    } else {
+      this.maxSeconds = 60;
+      if (maxValue) this.maxMinutes = Math.floor(maxValue / 60);
+    }
   }
 
   private convertSecondsToMinutes(totalSeconds: number): void {
