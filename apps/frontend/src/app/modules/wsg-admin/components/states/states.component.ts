ada3ed7ae2f922f3d7c92e0e4e9877c6f8d61cd2
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-11-15 17:11:15 +0100

    Refactor states component

--- apps/frontend/src/app/modules/wsg-admin/components/states/states.component.ts
+++ apps/frontend/src/app/modules/wsg-admin/components/states/states.component.ts
@@ -1,11 +1,12 @@
 import {
-  Component, EventEmitter, Output, Input, OnInit
+  Component, EventEmitter, Output, OnInit
 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
 import { State } from '../../../admin/models/state.type';
 import { WsgAdminService } from '../../services/wsg-admin.service';
 import { DeleteStateComponent } from '../delete-state/delete-state.component';
+import { BackendService } from '../../services/backend.service';
 
 @Component({
   selector: 'studio-lite-states',
@@ -14,75 +15,90 @@ import { DeleteStateComponent } from '../delete-state/delete-state.component';
 })
 export class StatesComponent implements OnInit {
   states: State[] = [];
-  selectionChanged!: State[];
-  @Output() hasChanged = new EventEmitter<State[]>();
-  @Input() statesInput:State[] = [];
+  changedStates!: State[];
+  isLoading: boolean = false;
+  isError: boolean = false;
 
+  @Output() hasChanged = new EventEmitter<State[]>();
+  @Output() stateDeleted = new EventEmitter<State[]>();
   constructor(
     private wsgAdminService: WsgAdminService,
     private deleteStateDialog: MatDialog,
-    private translateService:TranslateService
+    private translateService:TranslateService,
+    private backendService:BackendService
   ) {}
 
-  stateSelectionChange(e:any) {
-    const foundState = this.states?.find((state: { id: number; }) => state.id === parseInt(e.target.id, 10));
+  stateSelectionChange(value:string, type:string, id:string) {
+    const foundState = this.states
+      ?.find((state: { id: number; }) => state.id === parseInt(id, 10));
     if (foundState) {
-      this.selectionChanged = this.selectionChanged.map(state => {
+      this.changedStates = this.changedStates.map(state => {
         if (state.id === foundState.id) {
-          if (e.target.type === 'color') {
-            return { ...state, color: e.target?.value };
+          if (type === 'color') {
+            return { ...state, color: value };
           }
-          if (e.target.type === 'text') {
-            return { ...state, label: e.target?.value };
+          if (type === 'text') {
+            return { ...state, label: value };
           }
         }
         return state;
       });
     }
-    this.hasChanged.emit(this.selectionChanged);
+    this.hasChanged.emit(this.changedStates);
   }
 
   addState() {
-    if (this.selectionChanged) {
-      this.selectionChanged.push({
-        id: this.selectionChanged.length + 1,
+    if (this.changedStates.length) {
+      const newId = this.changedStates[this.changedStates.length - 1].id + 1;
+      this.changedStates.push({
+        id: newId,
         label: '',
         color: '#edb211'
       });
-      this.states = this.selectionChanged;
+      this.states = this.changedStates;
     } else {
-      this.selectionChanged = [{
+      this.changedStates = [{
         id: 1,
         label: '',
         color: '#edb211'
       }];
-      this.states = this.selectionChanged;
+      this.states = this.changedStates;
     }
-    this.hasChanged.emit(this.selectionChanged);
+    this.hasChanged.emit(this.changedStates);
   }
 
   deleteState(state:State) {
+    const deleteState = this.changedStates.filter((stateChanged:State) => stateChanged.id === state.id);
     const dialogRef = this.deleteStateDialog.open(DeleteStateComponent, {
       width: '600px',
       data: {
         title: this.translateService.instant('wsg-settings.delete-state'),
-        prompt: this.translateService.instant('wsg-settings.delete-state-warning', { state: 'SS' }),
-        state: state,
+        prompt: this.translateService.instant('wsg-settings.delete-state-warning'),
+        state: deleteState[0],
         okButtonLabel: this.translateService.instant('delete')
       }
     });
     dialogRef.afterClosed().subscribe(result => {
       if (result) {
-        this.states = this.states?.filter((s: State) => s.id !== state.id)
-          .map((s: State, index: number) => ({ ...s, id: index + 1 }));
-        this.selectionChanged = this.states as State[];
-        this.hasChanged.emit(this.selectionChanged);
+        this.states = this.states?.filter((s: State) => s.id !== state.id);
+        this.changedStates = this.states as State[];
+        this.backendService.deleteStateInWorkspaceGroupUnits(this.wsgAdminService.selectedWorkspaceGroupId, state.id);
+        this.hasChanged.emit(this.changedStates);
+        this.stateDeleted.emit(this.changedStates);
       }
     });
   }
 
   ngOnInit(): void {
+    if (!this.wsgAdminService.selectedWorkspaceGroupSettings.states) {
+      this.isLoading = true;
+      setTimeout(() => {
+        this.states = this.wsgAdminService.selectedWorkspaceGroupSettings.states || [];
+        this.changedStates = this.states;
+        this.isLoading = false;
+      }, 200);
+    }
     this.states = this.wsgAdminService.selectedWorkspaceGroupSettings.states || [];
-    this.selectionChanged = this.states;
+    this.changedStates = this.states;
   }
 }
