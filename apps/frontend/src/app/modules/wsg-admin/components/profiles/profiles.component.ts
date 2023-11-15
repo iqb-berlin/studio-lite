231166a06055e6ddbfd78c1d7110ab138f553ff9
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-11-15 17:09:01 +0100

    Refactor profiles component

--- apps/frontend/src/app/modules/wsg-admin/components/profiles/profiles.component.ts
+++ apps/frontend/src/app/modules/wsg-admin/components/profiles/profiles.component.ts
@@ -1,22 +1,14 @@
 import {
   Component, OnInit, Output, EventEmitter
 } from '@angular/core';
-
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatCheckboxChange } from '@angular/material/checkbox';
-
+import { MDProfileStore, MDProfile } from '@iqb/metadata';
 import { WsgAdminService } from '../../services/wsg-admin.service';
-import { State } from '../../../admin/models/state.type';
 
-type ProfileStore = {
-  url: string,
-  title: string,
-  description: string,
-  profiles: Array<Profile>
-};
-type Profile = {
-  id: string,
-  label: string
+export type CoreProfile = Omit<MDProfile, 'groups'>;
+export type ProfileStoreWithProfiles = {
+  profileStore:MDProfileStore,
+  profiles: MDProfile[]
 };
 
 @Component({
@@ -25,29 +17,19 @@ type Profile = {
   styleUrls: ['./profiles.component.scss']
 })
 export class ProfilesComponent implements OnInit {
-  selectionChanged!: State[];
-  editWorkspaceGroupSettingsForm: UntypedFormGroup;
-  profileStores : Array<ProfileStore> = [];
   isLoading: boolean = false;
   isError: boolean = false;
-  profiles: Array<Profile> = [];
-  profilesSelected : Profile[] = [];
-  data:any = { name: '', id: 1 };
-  fetchedProfiles: Profile[] = [];
-  stores:any = [];
   PROFILE_REGISTRY = 'https://raw.githubusercontent.com/iqb-vocabs/profile-registry/master/registry.csv';
+  storesURLs: string[] = [];
+  ProfileStoreWithProfilesCollection : ProfileStoreWithProfiles[] = [];
+  fetchedProfiles: CoreProfile[] = [];
+  profilesSelected : CoreProfile[] = [];
 
-  @Output() hasChanged = new EventEmitter<Array<Profile>>();
+  @Output() hasChanged = new EventEmitter<Array<CoreProfile>>();
 
   constructor(
-    private fb: UntypedFormBuilder,
     private wsgAdminService: WsgAdminService
-
-  ) {
-    this.editWorkspaceGroupSettingsForm = this.fb.group({
-      name: this.fb.control(this.data.name, [Validators.required, Validators.minLength(3)])
-    });
-  }
+  ) {}
 
   async ngOnInit(): Promise<void> {
     await this.readCsv();
@@ -58,101 +40,85 @@ export class ProfilesComponent implements OnInit {
   async readCsv() {
     try {
       if (this.wsgAdminService.profileStores) {
-        this.profileStores = this.wsgAdminService.profileStores;
+        this.ProfileStoreWithProfilesCollection = this.wsgAdminService.profileStores;
       } else {
-        const response = await fetch(this.PROFILE_REGISTRY);
-        if (response.ok) {
+        const profileRegistryResponse = await fetch(this.PROFILE_REGISTRY);
+        if (profileRegistryResponse.ok) {
           this.isLoading = true;
-          const data = await response.text();
-          this.csvToObj(data, '"');
+          const profileRegistry = await profileRegistryResponse.text();
+          this.csvToProfileURLs(profileRegistry, '"');
           // eslint-disable-next-line no-restricted-syntax
-          for await (const store of this.stores) {
-            const sanitizedUrl = store.url.replace(',', '');
-            store.profiles = await this.getProfiles(sanitizedUrl);
-            this.profileStores.push(store);
+          for await (const url of this.storesURLs) {
+            const response = await fetch(url);
+            if (response.ok) {
+              const store = await response.json();
+              if (store) {
+                const ProfilesStore = new MDProfileStore(store);
+                const profiles:MDProfile[] = [];
+                // eslint-disable-next-line no-restricted-syntax
+                for await (const profile of ProfilesStore.profiles) {
+                  const afterWith = (url.slice(0, url.lastIndexOf('/')));
+                  const pro = await this.getProfile(`${afterWith}/${profile}`);
+                  profiles.push(pro as MDProfile);
+                }
+                this.ProfileStoreWithProfilesCollection.push({
+                  profileStore: ProfilesStore,
+                  profiles: profiles
+                });
+              }
+            }
           }
-          this.wsgAdminService.profileStores = this.profileStores;
+          this.wsgAdminService.profileStores = this.ProfileStoreWithProfilesCollection;
           this.isLoading = false;
         }
       }
     } catch (err) {
-      console.log('ERR', err);
+      this.isError = true;
       this.isLoading = false;
     }
   }
 
-  // eslint-disable-next-line class-methods-use-this
-  async getProfile(profile:string) {
-    try {
-      const response = await fetch(`${profile}`);
-      if (response.ok) {
-        const data = await response.json();
-        return {
-          id: data.id,
-          label: data.label[0].value
-        };
-      }
-      return {
-        message: 'Profil konnte nicht geladen werden',
-        err: response.status
-      };
-    } catch (err) {
-      return {
-        message: 'Profil konnte nicht geladen werden',
-        err: err
-      };
-    }
-  }
-
-  async getProfiles(profileStore:string) {
-    try {
-      const response = await fetch(`${profileStore}`);
-      if (response.ok) {
-        const store = await response.json();
-        const profiles:any = [];
-        // eslint-disable-next-line no-restricted-syntax
-        for await (const profile of store.profiles) {
-          const afterWith = (profileStore.slice(0, profileStore.lastIndexOf('/')));
-          const pro = await this.getProfile(`${afterWith}/${profile}`);
-          profiles.push(pro);
-        }
-        return profiles;
-      }
-      return {
-        message: 'Profil konnte nicht geladen werden',
-        err: response.status
-      };
-    } catch (err) {
-      return {
-        message: 'Profil konnte nicht geladen werden',
-        err: err
-      };
-    }
-  }
-
-  csvToObj(stringVal:string, splitter:string) {
-    const [keys, ...rest] = stringVal
+  csvToProfileURLs(stringVal:string, splitter:string) {
+    const [, ...rest] = stringVal
       .trim()
       .split('\n')
       .map(item => item.split(splitter));
-    const filteredRest = rest.map(e => e.filter(el => (el !== ',' && el !== '')));
-    const splitKeys = keys[0].split(',');
-    this.stores = filteredRest.map(item => {
-      const object:any = {};
-      // eslint-disable-next-line no-return-assign
-      splitKeys.forEach((key, index) => (object[key] = item.at(index)));
-      return object;
-    });
+    const storesArray = rest.map(e => e.filter(el => (el !== ',' && el !== '')));
+    const storesURLs = [];
+    // eslint-disable-next-line no-restricted-syntax
+    for (const store of storesArray) {
+      const sanitizedURL = store[2].replace(',', '');
+      storesURLs.push(sanitizedURL);
+    }
+    this.storesURLs = storesURLs;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  async getProfile(profileURL:string) {
+    try {
+      const response = await fetch(`${profileURL}`);
+      if (response.ok) {
+        const data = await response.json();
+        return new MDProfile(data);
+      }
+      return [];
+    } catch {
+      this.isError = true;
+      this.isLoading = false;
+      return [];
+    }
   }
 
   isChecked(id:string):boolean {
     return !!this.fetchedProfiles?.find((profile: { id: string; }) => profile.id === id);
   }
 
-  changeSelection(e:MatCheckboxChange) {
-    e.checked ? this.profilesSelected.push({ id: e.source.name || '', label: e.source.id }) :
+  changeSelection(checkbox:MatCheckboxChange) {
+    checkbox.checked ?
+      this.profilesSelected.push(
+        { id: checkbox.source.id || '', label: checkbox.source.name || '' }) :
       this.profilesSelected = this.profilesSelected
-        .filter((profile: Profile) => profile.id !== e.source.name);
+        .filter((profile: CoreProfile) => profile.id !== checkbox.source.id);
     this.hasChanged.emit(this.profilesSelected);
   }
 }
