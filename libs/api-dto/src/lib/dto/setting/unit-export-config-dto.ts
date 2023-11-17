0435add822ee3276d950863330b9adb4ed5c4fd8
jurei733 <67505990+jurei733@users.noreply.github.com>
2023-11-17 11:36:52 +0100

    Update unit export config dto urls

--- libs/api-dto/src/lib/dto/setting/unit-export-config-dto.ts
+++ libs/api-dto/src/lib/dto/setting/unit-export-config-dto.ts
@@ -3,13 +3,13 @@ import { ApiProperty } from '@nestjs/swagger';
 export class UnitExportConfigDto {
   @ApiProperty()
     unitXsdUrl =
-      'https://raw.githubusercontent.com/iqb-berlin/testcenter-backend/master/definitions/vo_Unit.xsd';
+      'https://github.com/iqb-berlin/testcenter/blob/master/definitions/vo_Unit.xsd';
 
   @ApiProperty()
     bookletXsdUrl =
-      'https://raw.githubusercontent.com/iqb-berlin/testcenter-backend/master/definitions/vo_Booklet.xsd';
+      'https://github.com/iqb-berlin/testcenter/blob/master/definitions/vo_Booklet.xsd';
 
   @ApiProperty()
     testTakersXsdUrl =
-      'https://raw.githubusercontent.com/iqb-berlin/testcenter-backend/master/definitions/vo_Testtakers.xsd';
+      'https://github.com/iqb-berlin/testcenter/blob/master/definitions/vo_Testtakers.xsd';
 }
