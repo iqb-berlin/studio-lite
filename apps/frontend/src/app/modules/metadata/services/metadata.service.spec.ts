fc9e392556a84bad158491674826acdec47c9d62
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-12-18 18:05:08 +0100

    [frontend] Fix test for MetadataService

--- apps/frontend/src/app/modules/metadata/services/metadata.service.spec.ts
+++ apps/frontend/src/app/modules/metadata/services/metadata.service.spec.ts
@@ -1,11 +1,18 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { MetadataService } from './metadata.service';
+import { environment } from '../../../../environments/environment';
 
 describe('MetadataService', () => {
   let service: MetadataService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [{
+        provide: 'SERVER_URL',
+        useValue: environment.backendUrl
+      }]
     });
     service = TestBed.inject(MetadataService);
   });
