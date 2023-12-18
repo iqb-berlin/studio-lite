ef43e26b4f09450b377a319a014e967d44272c2d
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-12-18 17:56:59 +0100

    [frontend] Fix test for logout directive

--- apps/frontend/src/app/directives/logout.directive.spec.ts
+++ apps/frontend/src/app/directives/logout.directive.spec.ts
@@ -1,6 +1,7 @@
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { inject, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
+import { TranslateService } from '@ngx-translate/core';
 import { LogoutDirective } from './logout.directive';
 import { AppService } from '../services/app.service';
 import { BackendService } from '../services/backend.service';
@@ -22,8 +23,13 @@ describe('LogoutDirective', () => {
 
   it('should create an instance',
     inject([],
-      (appService: AppService, dialog: MatDialog, backendService: BackendService) => {
-        const directive = new LogoutDirective(appService, dialog, backendService);
+      (appService: AppService,
+       dialog: MatDialog,
+       backendService: BackendService,
+       translateService: TranslateService) => {
+        const directive = new LogoutDirective(
+          appService, dialog, backendService, translateService
+        );
         expect(directive).toBeTruthy();
       }));
 });
