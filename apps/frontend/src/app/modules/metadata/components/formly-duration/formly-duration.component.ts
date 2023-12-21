062e7a31bcd18c337dca5074184fc167778af0d9
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2023-12-21 12:52:38 +0100

    [frontend] Fix update of time information when changing units
    #577

--- apps/frontend/src/app/modules/metadata/components/formly-duration/formly-duration.component.ts
+++ apps/frontend/src/app/modules/metadata/components/formly-duration/formly-duration.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FieldType } from '@ngx-formly/material';
 import { FieldTypeConfig, FormlyFieldProps } from '@ngx-formly/core/public_api';
+import { Subject, takeUntil } from 'rxjs';
 import { Duration } from '../../models/duration.interface';
 import { DurationService } from '../../services/duration.service';
 
@@ -14,15 +15,21 @@ interface FormlyDurationProps extends FormlyFieldProps {
   templateUrl: './formly-duration.component.html',
   styleUrls: ['./formly-duration.component.scss']
 })
-export class FormlyDurationComponent extends FieldType<FieldTypeConfig<FormlyDurationProps>> implements OnInit {
+export class FormlyDurationComponent
+  extends FieldType<FieldTypeConfig<FormlyDurationProps>> implements OnInit, OnDestroy {
   duration: Duration = { minutes: '0', seconds: '0' };
   minSeconds = 0;
   maxSeconds!: number;
   minMinutes = 0;
   maxMinutes!: number;
+
+  private ngUnsubscribe = new Subject<void>();
   ngOnInit(): void {
     this.convertSecondsToMinutes(this.formControl.value || 0);
     this.setMinMaxValues();
+    this.formControl.valueChanges
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(v => this.convertSecondsToMinutes(v || 0));
   }
 
   private setMinMaxValues(): void {
@@ -58,5 +65,11 @@ export class FormlyDurationComponent extends FieldType<FieldTypeConfig<FormlyDur
     const total = this.convertMinutesToSeconds();
     this.convertSecondsToMinutes(total);
     this.formControl.setValue(total);
+  }
+
+  override ngOnDestroy(): void {
+    super.ngOnDestroy();
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
