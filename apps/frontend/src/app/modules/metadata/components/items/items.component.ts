327cac719f3cdca9e2fc3145011c2fbfcd523f8b
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-01-25 14:07:09 +0100

    Fix unit tests

--- apps/frontend/src/app/modules/metadata/components/items/items.component.ts
+++ apps/frontend/src/app/modules/metadata/components/items/items.component.ts
@@ -5,8 +5,6 @@ import {
   BehaviorSubject, Subject, takeUntil
 } from 'rxjs';
 
-import { MatDialog } from '@angular/material/dialog';
-
 @Component({
   selector: 'studio-lite-items',
   templateUrl: './items.component.html',
@@ -26,9 +24,6 @@ export class ItemsComponent implements OnInit, OnChanges, OnDestroy {
   @Input() language!: string;
 
   @Output() metadataChange: EventEmitter<any> = new EventEmitter();
-
-  constructor(public dialog: MatDialog) {
-  }
 
   ngOnInit(): void {
     this.variablesLoader
