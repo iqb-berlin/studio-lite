df89e95aa179945af53f7052ae2667406ab518ce
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2024-03-26 10:28:47 +0100

    [frontend] Fix test for UserMenuComponent

--- apps/frontend/src/app/components/user-menu/user-menu.component.spec.ts
+++ apps/frontend/src/app/components/user-menu/user-menu.component.spec.ts
@@ -1,50 +1,48 @@
 // eslint-disable-next-line max-classes-per-file
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TranslateModule } from '@ngx-translate/core';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
 import { Component, Input } from '@angular/core';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { HttpClientModule } from '@angular/common/http';
 import { UserMenuComponent } from './user-menu.component';
-import { AuthService } from '../../../../../api/src/app/auth/service/auth.service';
+import { WrappedIconComponent } from '../../modules/shared/components/wrapped-icon/wrapped-icon.component';
+import { AccountActionComponent } from '../account-action/account-action.component';
+import { ChangePasswordDirective } from '../../directives/change-password.directive';
+import { EditMyDataDirective } from '../../directives/edit-my-data.directive';
+import { LogoutDirective } from '../../directives/logout.directive';
 
-class MockAuthService {
-
-}
 describe('UserMenuComponent', () => {
   let component: UserMenuComponent;
   let fixture: ComponentFixture<UserMenuComponent>;
 
-  @Component({ selector: 'studio-lite-account-action', template: '' })
+  @Component({ selector: 'studio-lite-account-action', standalone: true, template: '' })
   class MockAccountActionComponentComponent {
     @Input() type!: string;
     @Input() iconName!: string;
   }
 
+  @Component({ selector: 'studio-lite-wrapped-icon', standalone: true, template: '' })
+  class MockWrappedIconComponent {
+    @Input() icon!: string;
+  }
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        MockAccountActionComponentComponent],
       imports: [
-        MatTooltipModule,
-        MatIconModule,
-        MatMenuModule,
-        HttpClientModule,
+        UserMenuComponent,
         TranslateModule.forRoot()
-      ],
-      providers: [
-        {
-          provide: AuthService,
-          useClass: MockAuthService
-        },
-        {
-          provide: 'SERVER_URL',
-          useValue: 'http://localhost:3333'
-        }
       ]
-    }).compileComponents();
-
+    }).overrideComponent(UserMenuComponent, {
+      remove: {
+        imports: [
+          WrappedIconComponent,
+          AccountActionComponent,
+          ChangePasswordDirective,
+          EditMyDataDirective,
+          LogoutDirective
+        ]
+      },
+      add: { imports: [MockAccountActionComponentComponent, MockWrappedIconComponent] }
+    })
+      .compileComponents();
     fixture = TestBed.createComponent(UserMenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
