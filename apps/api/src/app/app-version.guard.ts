47e5061efe4e58a988af3c191d599a3c923a6fc7
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-04-03 12:00:48 +0200

    Increase version to 7.1.0

--- apps/api/src/app/app-version.guard.ts
+++ apps/api/src/app/app-version.guard.ts
@@ -27,5 +27,5 @@ export class AppVersionGuard implements CanActivate {
 
 export const AppVersionProvider = {
   provide: 'APP_VERSION',
-  useValue: '7.0.0'
+  useValue: '7.1.0'
 };
--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -90,7 +90,7 @@ bootstrapApplication(AppComponent, {
     },
     {
       provide: 'APP_VERSION',
-      useValue: '7.0.0'
+      useValue: '7.1.0'
     },
     provideAnimations(),
     provideHttpClient(withInterceptorsFromDi())
