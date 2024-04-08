1940e92f6e06162153fc2ff8db9db67940d77dd4
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2024-04-08 17:45:56 +0200

    [frontend] Add test files for metadata pipes

---
+++ apps/frontend/src/app/modules/metadata/pipes/is-node-selected.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { IsNodeSelectedPipe } from './is-node-selected.pipe';
+
+describe('IsNodeSelectedPipe', () => {
+  it('should be defined', () => {
+    expect(new IsNodeSelectedPipe()).toBeDefined();
+  });
+});
---
+++ apps/frontend/src/app/modules/metadata/pipes/is-tree-control-expanded.pipe.spec.ts
@@ -0,0 +1,7 @@
+import { IsTreeControlExpandedPipe } from './is-tree-control-expanded.pipe';
+
+describe('IsTreeControlExpandedPipe', () => {
+  it('should be defined', () => {
+    expect(new IsTreeControlExpandedPipe()).toBeDefined();
+  });
+});
