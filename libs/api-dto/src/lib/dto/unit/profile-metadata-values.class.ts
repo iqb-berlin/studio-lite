f351720c78252a7178dede381ab8d82344d514a1
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2024-04-08 16:45:10 +0200

    [frontend] Add classes and types for metadata

---
+++ libs/api-dto/src/lib/dto/unit/profile-metadata-values.class.ts
@@ -0,0 +1,32 @@
+// eslint-disable-next-line max-classes-per-file
+import { TextsWithLanguageAndId } from '@iqb/metadata/md-values';
+import { TextWithLanguage } from '@iqb/metadata/md-main';
+
+export class ProfileMetadataValues {
+  profiles?: MetadataValues[];
+}
+
+export class UnitMetadataValues extends ProfileMetadataValues {
+  items?: ItemsMetadataValues[];
+}
+
+export class MetadataValues {
+  entries?: MetadataValuesEntry[];
+  profileId?: string;
+  isCurrent?: boolean;
+}
+
+export class ItemsMetadataValues extends ProfileMetadataValues {
+  id?: string;
+  description?: string;
+  variableId?: string;
+  weighting?: number;
+  [key: string]: string | number | MetadataValues[] | undefined;
+}
+
+export class MetadataValuesEntry {
+  id!: string;
+  label!: TextWithLanguage[];
+  value!: TextsWithLanguageAndId[] | TextWithLanguage[] | string;
+  valueAsText!: TextWithLanguage | TextWithLanguage[];
+}
