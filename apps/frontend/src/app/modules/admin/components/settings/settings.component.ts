b63f1ca2650941b21d21963d78cd07ce34bf328e
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-04-23 14:30:51 +0200

    Add missings profiles config to admin settings

--- apps/frontend/src/app/modules/admin/components/settings/settings.component.ts
+++ apps/frontend/src/app/modules/admin/components/settings/settings.component.ts
@@ -4,12 +4,20 @@ import { MatLabel } from '@angular/material/form-field';
 import { UnitExportConfigComponent } from '../unit-export-config/unit-export-config.component';
 import { AppLogoComponent } from '../app-logo/app-logo.component';
 import { AppConfigComponent } from '../app-config/app-config.component';
+import { MissingsProfilesConfigComponent } from '../missings-profiles-config/missings-profiles-config.component';
 
 @Component({
   selector: 'studio-lite-settings',
   templateUrl: './settings.component.html',
   styleUrls: ['./settings.component.scss'],
   standalone: true,
-  imports: [MatLabel, AppConfigComponent, AppLogoComponent, UnitExportConfigComponent, TranslateModule]
+  imports: [
+    MatLabel,
+    AppConfigComponent,
+    AppLogoComponent,
+    UnitExportConfigComponent,
+    TranslateModule,
+    MissingsProfilesConfigComponent
+  ]
 })
 export class SettingsComponent {}
