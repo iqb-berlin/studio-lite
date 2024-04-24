2b03bcd572d80b5e3d0652117f065eb1bf5f20d6
jurei733 <67505990+jurei733@users.noreply.github.com>
2024-04-24 17:27:37 +0200

    Increase to version 7.3.1

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -90,7 +90,7 @@ bootstrapApplication(AppComponent, {
     },
     {
       provide: 'APP_VERSION',
-      useValue: '7.3.0'
+      useValue: '7.3.1'
     },
     provideAnimations(),
     provideHttpClient(withInterceptorsFromDi())
