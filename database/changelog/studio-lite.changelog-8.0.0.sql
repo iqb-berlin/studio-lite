821cf0fbe5b827c770252a3de0f10e7b605e5016
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2024-06-28 10:06:39 +0200

    [db] Add migration for write_access_level

---
+++ database/changelog/studio-lite.changelog-8.0.0.sql
@@ -0,0 +1,19 @@
+-- liquibase formatted sql
+
+-- changeset jojohoch:1
+ALTER TABLE "public"."workspace_user"
+  ALTER "has_write_access" SET DEFAULT null;
+-- rollback ALTER TABLE "public"."workspace_user" SET DEFAULT TRUE;
+
+
+-- changeset jojohoch:2
+ALTER TABLE "public"."workspace_user"
+  ALTER "has_write_access" TYPE INTEGER USING CASE WHEN "has_write_access" THEN 1 ELSE 0 END;
+-- rollback ALTER TABLE "public"."workspace_user" ALTER "has_write_access" TYPE BOOLEAN USING CASE WHEN has_write_access=0 THEN FALSE ELSE TRUE END;
+
+
+-- changeset jojohoch:3
+ALTER TABLE "public"."workspace_user"
+  RENAME "has_write_access" TO "write_access_level";
+-- rollback ALTER TABLE "public"."workspace_user" RENAME "write_access_level" TO "has_write_access";
+
