790c95db42db63d8789338d2d70f8f0523ca063f
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2024-07-05 10:06:09 +0200

    [frontend] Change writing style of value checks for accessLevel

--- apps/frontend/src/app/modules/workspace/guards/unit-routing.guard.ts
+++ apps/frontend/src/app/modules/workspace/guards/unit-routing.guard.ts
@@ -18,7 +18,7 @@ export class UnitRoutingCanDeactivateGuard {
   ) { }
 
   canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
-    if (this.workspaceService.isChanged() && this.workspaceService.userAccessLevel) {
+    if (this.workspaceService.isChanged() && this.workspaceService.userAccessLevel < 1) {
       let content = '';
       let isWarning = true;
       this.workspaceService.isValidFormKey.subscribe(isValid => {
