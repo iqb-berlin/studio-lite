6449b1c7f3d5e37dced0696a8acfce47fea2fd95
svwolter <sven.wolter@iqb.hu-berlin.de>
2024-07-11 13:58:27 +0200

    Fix 'make lint-...' commands

--- scripts/make/lint.mk
+++ scripts/make/lint.mk
@@ -8,12 +8,12 @@ lint-app: lint-backend lint-frontend lint-frontend-e2e
 
 ## Run backend linter (only in combination with 'make dev-up')
 lint-backend:
-	docker compose --env-file $(STUDIO_LITE_BASE_DIR)/.env.dev exec -it backend bash -c "nx lint api"
+	docker compose --env-file $(STUDIO_LITE_BASE_DIR)/.env.dev exec -it backend bash -c "npx nx lint api"
 
 ## Run frontend linter (only in combination with 'make dev-up')
 lint-frontend:
-	docker compose --env-file $(STUDIO_LITE_BASE_DIR)/.env.dev exec -it frontend bash -c "nx lint frontend"
+	docker compose --env-file $(STUDIO_LITE_BASE_DIR)/.env.dev exec -it frontend bash -c "npx nx lint frontend"
 
 ## Run frontend-e2e linter (only in combination with 'make dev-up')
 lint-frontend-e2e:
-	docker compose --env-file $(STUDIO_LITE_BASE_DIR)/.env.dev exec -it frontend bash -c "nx lint frontend-e2e"
+	docker compose --env-file $(STUDIO_LITE_BASE_DIR)/.env.dev exec -it frontend bash -c "npx nx lint frontend-e2e"
