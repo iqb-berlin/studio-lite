80b3c33475e1dedf96d195129922c84b6d1021f7
nanoyan <yanpingchenlin@gmail.com>
2024-07-12 13:25:32 +0200

    [e2e] Api unit split negative and positive text with context

--- apps/frontend-e2e/src/e2e/api/api-unit.cy.ts
+++ apps/frontend-e2e/src/e2e/api/api-unit.cy.ts
@@ -1,7 +1,15 @@
 import {
-  addFirstUserAPI, createGroupAPI, createWsAPI, deleteFirstUserAPI, deleteGroupAPI, getUserIdAPI, setAdminOfGroupAPI
+  addFirstUserAPI,
+  createGroupAPI,
+  createWsAPI,
+  deleteFirstUserAPI,
+  deleteGroupAPI,
+  getUserIdAPI, grantAccessWsAPI,
+  setAdminOfGroupAPI
 } from '../../support/utilAPI';
-import { GroupData, UnitData, WsData } from '../../support/testData';
+import {
+  AccessLevel, GroupData, UnitData, WsData
+} from '../../support/testData';
 
 describe('API unit tests', () => {
   const group1: GroupData = {
@@ -11,6 +19,10 @@ describe('API unit tests', () => {
   const ws1: WsData = {
     id: 'id_ws1',
     name: '01Vorlage'
+  };
+  const ws2: WsData = {
+    id: 'id_ws2',
+    name: 'NoAccess'
   };
   const unit1: UnitData = {
     shortname: 'D01',
@@ -34,16 +46,51 @@ describe('API unit tests', () => {
     it('e. Create a workspace', () => {
       createWsAPI(Cypress.env(group1.id), ws1);
     });
-    it.skip('1. Create a Unit POST /api/workspace/id_workspace/units`', () => {
-      console.log(Cypress.env(ws1.id));
+    it('f. Grant admin privileges in the workspace', () => {
+      grantAccessWsAPI(Cypress.env(ws1.id), Cypress.env('id_admin'), AccessLevel.Admin);
+    });
+    it('g. G Create a workspace w2', () => {
+      createWsAPI(Cypress.env(group1.id), ws2);
+    });
+  });
+  context('Positive tests', () => {
+    // 32
+    it('1. Create a Unit POST /api/workspace/id_workspace/units`', () => {
       cy.createUnitAPI(unit1, Cypress.env(ws1.id))
         .then(resp => {
-          expect(resp.status).to.equal(201);
+          expect(resp.status)
+            .to
+            .equal(201);
+          Cypress.env(unit1.shortname, resp.body);
+        });
+    });
+    // 33
+    it('2. Delete a Unit POST /api/workspace/id_workspace/ids`', () => {
+      cy.deleteUnitAPI(Cypress.env(unit1.shortname), Cypress.env(ws1.id))
+        .then(resp => {
+          expect(resp.status).to.equal(200);
           Cypress.env(unit1.shortname, resp.body);
         });
     });
   });
-
+  context('Negative tests', () => {
+    // 32
+    it('1. Create a Unit POST /api/workspace/id_workspace/units`', () => {
+      cy.createUnitAPI(unit1, Cypress.env(ws2.id))
+        .then(resp => {
+          expect(resp.status)
+            .to
+            .equal(403);
+        });
+    });
+    // 33
+    it('2. Delete a Unit POST /api/workspace/id_workspace/ids`', () => {
+      cy.deleteUnitAPI(Cypress.env(unit1.shortname), Cypress.env(ws2.id))
+        .then(resp => {
+          expect(resp.status).to.equal(404);
+        });
+    });
+  });
   context('Delete the context', () => {
     it('a. Delete the group', () => {
       deleteGroupAPI(Cypress.env(group1.id));
