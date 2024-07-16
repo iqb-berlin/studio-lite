c5c3b204543834a3c5c3ead99363e448832aa5d4
nanoyan <yanpingchenlin@gmail.com>
2024-07-16 17:32:06 +0200

    [e2e] Add modules api

--- apps/frontend-e2e/src/support/commands-verona-api.ts
+++ apps/frontend-e2e/src/support/commands-verona-api.ts
@@ -2,6 +2,9 @@ Cypress.Commands.add('addModuleAPI', (module:string) => {
   const authorization = `bearer ${Cypress.env('token_admin')}`;
   cy.readFile(`../frontend-e2e/src/fixtures/${module}`)
     .then(aspect => {
+      const bodyToSend = {
+        aspect
+      };
       cy.request({
         method: 'POST',
         url: '/api/admin/verona-modules',
@@ -9,9 +12,20 @@ Cypress.Commands.add('addModuleAPI', (module:string) => {
           'app-version': Cypress.env('version'),
           authorization
         },
-        body: {
-          aspect
-        }
+        body: bodyToSend
       });
     });
+});
+
+Cypress.Commands.add('getModulesAPI', (token:string) => {
+  const authorization = `bearer ${token}`;
+  cy.request({
+    method: 'GET',
+    url: '/api/verona-modules',
+    headers: {
+      'app-version': Cypress.env('version'),
+      authorization
+    },
+    failOnStatusCode: false
+  });
 });
