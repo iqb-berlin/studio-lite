36982d4330fe08d0f43a235f21aa354cb5d2b7b2
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2024-08-30 09:57:59 +0200

    Fix disabled state of return unit button

--- apps/frontend/src/app/modules/workspace/pipes/can-return-unit.pipe.ts
+++ apps/frontend/src/app/modules/workspace/pipes/can-return-unit.pipe.ts
@@ -9,7 +9,7 @@ export class CanReturnUnitPipe implements PipeTransform {
   // eslint-disable-next-line class-methods-use-this
   transform(unitId: number, unitList: { [key: string]: UnitInListDto[] }): boolean {
     const unit = Object.values(unitList).flat().find(u => u.id === unitId);
-    if (unit) return !!unit.sourceWorkspaceId;
+    if (unit) return !!unit.sourceWorkspaceId && !unit.returned;
     return false;
   }
 }
