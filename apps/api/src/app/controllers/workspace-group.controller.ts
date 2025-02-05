89a52e053f59570a6be91623bb7d41acbc006c83
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2025-02-05 14:46:04 +0100

    Remove the superfluous call and endpoint for deleting states

--- apps/api/src/app/controllers/workspace-group.controller.ts
+++ apps/api/src/app/controllers/workspace-group.controller.ts
@@ -1,6 +1,6 @@
 import {
   Body,
-  Controller, Delete, Get, Param, Patch, UseGuards
+  Controller, Get, Patch, UseGuards
 } from '@nestjs/common';
 import {
   ApiBearerAuth, ApiNotFoundResponse, ApiOkResponse, ApiParam, ApiTags
@@ -13,13 +13,11 @@ import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { WorkspaceGroupService } from '../services/workspace-group.service';
 import { WorkspaceGroupId } from '../decorators/workspace-group.decorator';
 import { IsWorkspaceGroupAdminGuard } from '../guards/is-workspace-group-admin.guard';
-import { UnitService } from '../services/unit.service';
 
 @Controller('workspace-groups/:workspace_group_id')
 export class WorkspaceGroupController {
   constructor(
-    private workspaceGroupService: WorkspaceGroupService,
-    private unitService:UnitService
+    private workspaceGroupService: WorkspaceGroupService
   ) {}
 
   @Get()
@@ -39,16 +37,5 @@ export class WorkspaceGroupController {
   @ApiTags('workspace-group')
   async patch(@Body() workspaceGroupFullDto: WorkspaceGroupFullDto) {
     return this.workspaceGroupService.patch(workspaceGroupFullDto);
-  }
-
-  @Delete(':state_id')
-  @UseGuards(JwtAuthGuard, IsWorkspaceGroupAdminGuard)
-  @ApiParam({ name: 'workspace_group_id', type: Number })
-  @ApiBearerAuth()
-  @ApiOkResponse({ description: 'State removed from workspace group successfully.' })
-  @ApiNotFoundResponse({ description: 'Workspace group not found.' })
-  @ApiTags('workspace-group')
-  async removeState(@WorkspaceGroupId() id: number, @Param('state_id') state_id: string): Promise<void> {
-    return this.unitService.deleteState(id, state_id);
   }
 }
