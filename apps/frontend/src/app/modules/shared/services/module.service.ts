0e6374f31febc00143959c20b08f37edaaa60cb4
jurei733 <67505990+jurei733@users.noreply.github.com>
2025-03-27 15:19:52 +0100

    Refactor module service

--- apps/frontend/src/app/modules/shared/services/module.service.ts
+++ apps/frontend/src/app/modules/shared/services/module.service.ts
@@ -16,35 +16,54 @@ export class ModuleService {
     private backendService: BackendService
   ) {}
 
-  async loadList() {
-    const newEditors: { [key: string]: VeronaModuleClass } = {};
-    const newPlayers: { [key: string]: VeronaModuleClass } = {};
-    const newSchemers: { [key: string]: VeronaModuleClass } = {};
-    const modules = await lastValueFrom(this.backendService.getModuleList());
-    if (modules) {
-      modules.forEach(m => {
-        const moduleObject = new VeronaModuleClass(m);
-        if (moduleObject.metadata.type === 'editor') {
-          newEditors[moduleObject.key] = moduleObject;
-        } else if (moduleObject.metadata.type === 'player') {
-          newPlayers[moduleObject.key] = moduleObject;
-        } else if (moduleObject.metadata.type === 'schemer') {
-          newSchemers[moduleObject.key] = moduleObject;
+  async loadList(): Promise<boolean> {
+    try {
+      const modules = await lastValueFrom(this.backendService.getModuleList());
+      if (!modules) {
+        return false;
+      }
+
+      const newModules: Record<'editor' | 'player' | 'schemer', { [key: string]: VeronaModuleClass }> = {
+        editor: {},
+        player: {},
+        schemer: {}
+      };
+
+      modules.forEach(module => {
+        const moduleObject = new VeronaModuleClass(module);
+        const type = moduleObject.metadata.type;
+
+        if (type === 'editor' || type === 'player' || type === 'schemer') {
+          newModules[type][moduleObject.key] = moduleObject;
         }
       });
+
+      this.editors = newModules.editor;
+      this.players = newModules.player;
+      this.schemers = newModules.schemer;
+
+      return true;
+    } catch (error) {
+      console.error('Fehler beim Laden der Modul-Liste:', error);
+      return false;
     }
-    this.editors = newEditors;
-    this.players = newPlayers;
-    this.schemers = newSchemers;
   }
 
   async getModuleHtml(module: VeronaModuleClass): Promise<string> {
-    if (module.html) return module.html;
-    const fileData = await lastValueFrom(this.backendService.getModuleHtml(module.key));
-    if (fileData) {
-      module.html = fileData.file;
+    if (module.html) {
       return module.html;
     }
+
+    try {
+      const fileData = await lastValueFrom(this.backendService.getModuleHtml(module.key));
+      if (fileData?.file) {
+        module.html = fileData.file; // HTML im Modul-Cache speichern
+        return module.html;
+      }
+    } catch (error) {
+      console.error('Fehler beim Abrufen des Module-HTML:', error);
+    }
+
     return '';
   }
 }
