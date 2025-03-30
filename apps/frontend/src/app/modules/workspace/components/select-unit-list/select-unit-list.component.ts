be0e67a3a9beb2a94df757bf8f5ea0d25c50c17f
jurei733 <67505990+jurei733@users.noreply.github.com>
2025-03-30 10:15:18 +0200

    Show selected unit to delete and refactor component

--- apps/frontend/src/app/modules/workspace/components/select-unit-list/select-unit-list.component.ts
+++ apps/frontend/src/app/modules/workspace/components/select-unit-list/select-unit-list.component.ts
@@ -1,5 +1,6 @@
 import {
-  Component, EventEmitter, Input, OnChanges, OnDestroy, Output, SimpleChanges, ViewChild
+  AfterViewInit,
+  Component, ElementRef, EventEmitter, Input, OnChanges, OnDestroy, Output, SimpleChanges, ViewChild
 } from '@angular/core';
 import {
   // eslint-disable-next-line max-len
@@ -27,63 +28,25 @@ import { SearchFilterComponent } from '../../../shared/components/search-filter/
   // eslint-disable-next-line max-len
   imports: [SearchFilterComponent, MatTable, MatSort, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatCheckbox, MatCellDef, MatCell, MatSortHeader, MatHeaderRowDef, MatHeaderRow, MatRowDef, MatRow, IncludePipe, IsSelectedPipe, IsAllSelectedPipe, HasSelectionValuePipe, TranslateModule]
 })
-export class SelectUnitListComponent implements OnChanges, OnDestroy {
+export class SelectUnitListComponent implements OnChanges, OnDestroy, AfterViewInit {
   objectsDatasource = new MatTableDataSource<UnitInListDto>();
   displayedColumns = ['selectCheckbox', 'key', 'name', 'groupName'];
   tableSelectionCheckboxes = new SelectionModel <UnitInListDto>(true, []);
   disabledUnits: number[] = [];
   selectionChangedSubscription: Subscription | null = null;
+  multipleSelection = true;
+
+  constructor(private backendService: BackendService) {
+    this.selectionChangedSubscription = this.tableSelectionCheckboxes.changed
+      .subscribe(() => this.selectionChanged.emit(this.selectedUnitIds));
+  }
 
   @Input() filter!: number[];
   @Input() initialSelection!: number[];
-  @Input() selectedUnitId!: number;
+  @Input() unitToBeDeletedId!: number;
   @Input() queryParams!: HttpParams;
   @Input() workspace!: number;
-
-  @Input('show-groups')
-  set showGroups(value: boolean) {
-    this.displayedColumns = value ? ['selectCheckbox', 'key', 'groupName'] : ['selectCheckbox', 'key'];
-  }
-
-  updateUnitList(value: number) {
-    this.tableSelectionCheckboxes.clear();
-    this.backendService.getUnitList(value, this.queryParams)
-      .subscribe(units => {
-        this.setObjectsDatasource(units);
-        this.setInitialSelection();
-      });
-  }
-
-  private setObjectsDatasource(units: UnitInListDto[]): void {
-    if (this.filter && this.filter.length) {
-      this.objectsDatasource = new MatTableDataSource(
-        units
-          .filter(unit => this.filter.indexOf(unit.id) > -1)
-      );
-    } else {
-      this.objectsDatasource = new MatTableDataSource(units);
-    }
-    this.objectsDatasource
-      .filterPredicate = (unitList: UnitInListDto, filter) => ['key', 'name', 'groupName']
-        .some(column => (unitList[column as keyof UnitInListDto] as string || '')
-          .toLowerCase()
-          .includes(filter)
-        );
-
-    this.objectsDatasource.sort = this.sort;
-    this.objectsDatasource.data.forEach(row => {
-      if (row.id === this.selectedUnitId) this.tableSelectionCheckboxes.select(row);
-    });
-  }
-
-  private setInitialSelection(): void {
-    if (this.initialSelection && this.initialSelection.length) {
-      this.selectedUnitIds = this.initialSelection;
-      this.selectionChanged.emit(this.selectedUnitIds);
-    }
-  }
-
-  multipleSelection = true;
+  @Input() selectedUnitIds: number[] = [];
   @Input('multiple')
   set multiple(value: boolean) {
     this.multipleSelection = value;
@@ -99,23 +62,115 @@ export class SelectUnitListComponent implements OnChanges, OnDestroy {
   }
 
   @Output() selectionChanged = new EventEmitter<number[]>();
+  @Input('show-groups')
+  set showGroups(value: boolean) {
+    this.displayedColumns = value ? ['selectCheckbox', 'key', 'groupName'] : ['selectCheckbox', 'key'];
+  }
+
+  @ViewChild('tableContainer') tableContainer!: ElementRef;
+  @ViewChild(MatSort) sort = new MatSort();
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const { workspace, unitToBeDeletedId, selectedUnitIds } = changes;
+    if (workspace && workspace.currentValue) this.updateUnitList(workspace.currentValue);
+    if (unitToBeDeletedId && unitToBeDeletedId.currentValue) {
+      this.scrollToUnit(unitToBeDeletedId.currentValue);
+    }
+    if (selectedUnitIds && selectedUnitIds.currentValue) {
+      this.updateSelection(selectedUnitIds.currentValue);
+    }
+  }
+
+  updateUnitList(value: number): void {
+    this.tableSelectionCheckboxes.clear();
+    this.backendService.getUnitList(value, this.queryParams)
+      .subscribe(units => {
+        this.setObjectsDatasource(units);
+        this.setInitialSelection();
+        if (this.unitToBeDeletedId) {
+          this.scrollToUnit(this.unitToBeDeletedId);
+        }
+      });
+  }
+
+  /**
+   * Methode zum Scrollen zur bestimmten Einheit in der Tabelle
+   * @param unitId ID der Einheit
+   */
+  scrollToUnit(unitId: number): void {
+    if (this.objectsDatasource && this.objectsDatasource.data.length > 0) {
+      const unitIndex = this.objectsDatasource.data
+        .findIndex(unit => unit.id === unitId);
+      if (unitIndex !== -1) {
+        const tableRows = this.tableContainer.nativeElement.querySelectorAll('mat-row');
+        const targetRow = tableRows[unitIndex];
+        if (targetRow) {
+          targetRow.scrollIntoView({ block: 'center' });
+        }
+      }
+    }
+  }
+
+  ngAfterViewInit(): void {
+    if (this.unitToBeDeletedId) {
+      setTimeout(() => {
+        this.scrollToUnit(this.unitToBeDeletedId);
+      }, 200
+      );
+    }
+  }
+
+  private setObjectsDatasource(units: UnitInListDto[]): void {
+    const filteredUnits = this.filter?.length ?
+      units.filter(unit => this.filter.includes(unit.id)) :
+      units;
+
+    this.objectsDatasource = new MatTableDataSource(filteredUnits);
+
+    this.objectsDatasource
+      .filterPredicate = (unitList: UnitInListDto, filter: string) => ['key', 'name', 'groupName']
+        .some(column => (unitList[column as keyof UnitInListDto] as string || '')
+          .toLowerCase()
+          .includes(filter.toLowerCase())
+        );
+
+    this.objectsDatasource.sort = this.sort;
+
+    const rowToBeDeleted = this.objectsDatasource.data
+      .find(row => row.id === this.unitToBeDeletedId);
+    if (rowToBeDeleted) {
+      this.tableSelectionCheckboxes.select(rowToBeDeleted);
+    }
+  }
+
+  private setInitialSelection(): void {
+    if (this.initialSelection && this.initialSelection.length) {
+      this.selectedUnitIds = this.initialSelection;
+      this.selectionChanged.emit(this.selectedUnitIds);
+    }
+  }
 
   get selectionCount(): number {
     return this.tableSelectionCheckboxes.selected.length;
   }
 
-  get selectedUnitIds(): number[] {
-    return this.tableSelectionCheckboxes.selected.map(ud => ud.id);
-  }
-
-  // TODO move to ngChanges
-  @Input('selectedUnitIds')
-  set selectedUnitIds(newUnits: number[]) {
-    if (this.selectionChangedSubscription) this.selectionChangedSubscription.unsubscribe();
+  private updateSelection(newUnits: number[]): void {
+    if (!newUnits || newUnits.length === 0) {
+      if (this.selectionChangedSubscription) {
+        this.selectionChangedSubscription.unsubscribe();
+        this.selectionChangedSubscription = null;
+      }
+      this.tableSelectionCheckboxes.clear();
+      return;
+    }
+    if (this.selectionChangedSubscription) {
+      this.selectionChangedSubscription.unsubscribe();
+      this.selectionChangedSubscription = null;
+    }
     this.tableSelectionCheckboxes.clear();
-    this.objectsDatasource.data.forEach(row => {
-      if (newUnits.includes(row.id)) this.tableSelectionCheckboxes.select(row);
-    });
+    const data = this.objectsDatasource.data;
+    const selectedRows = data.filter(row => newUnits.includes(row.id));
+    selectedRows.forEach(row => this.tableSelectionCheckboxes.select(row));
     this.selectionChangedSubscription = this.tableSelectionCheckboxes.changed.subscribe(() => {
       this.selectionChanged.emit(this.selectedUnitIds);
     });
@@ -133,13 +188,6 @@ export class SelectUnitListComponent implements OnChanges, OnDestroy {
     return '';
   }
 
-  @ViewChild(MatSort) sort = new MatSort();
-
-  constructor(private backendService: BackendService) {
-    this.selectionChangedSubscription = this.tableSelectionCheckboxes.changed
-      .subscribe(() => this.selectionChanged.emit(this.selectedUnitIds));
-  }
-
   private isAllSelected(): boolean {
     const numSelected = this.tableSelectionCheckboxes.selected.length;
     const numRows = this.objectsDatasource ? this.objectsDatasource.data.length : 0;
@@ -154,10 +202,5 @@ export class SelectUnitListComponent implements OnChanges, OnDestroy {
 
   ngOnDestroy(): void {
     if (this.selectionChangedSubscription) this.selectionChangedSubscription.unsubscribe();
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    const { workspace } = changes;
-    if (workspace && workspace.currentValue) this.updateUnitList(workspace.currentValue);
   }
 }
