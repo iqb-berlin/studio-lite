a2531b6b76bec72927a90ad67539cd80dcbff65f
svwolter <sven.wolter@iqb.hu-berlin.de>
2025-04-01 15:25:05 +0200

    Enrich 'restore' command comments

--- scripts/make/audit.mk
+++ scripts/make/audit.mk
@@ -1,15 +1,15 @@
 STUDIO_BASE_DIR := $(shell git rev-parse --show-toplevel)
 
-## prevents collisions of make target names with possible file names
+# prevents collisions of make target names with possible file names
 .PHONY: audit-app audit-backend audit-frontend
 
-## Run all audits (only in combination with 'make dev-up')
+# Run all audits (only in combination with 'make dev-up')
 audit-app: audit-backend audit-frontend
 
-## Run backend audit (only in combination with 'make dev-up')
+# Run backend audit (only in combination with 'make dev-up')
 audit-backend:
 	docker compose --env-file $(STUDIO_BASE_DIR)/.env.dev exec -it backend npm audit --audit-level critical
 
-## Run frontend audit (only in combination with 'make dev-up')
+# Run frontend audit (only in combination with 'make dev-up')
 audit-frontend:
 	docker compose --env-file $(STUDIO_BASE_DIR)/.env.dev exec -it frontend npm audit --audit-level critical
