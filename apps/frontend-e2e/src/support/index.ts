7672b0794eadf89036c1dc023dcc82cd41000725
Yan Ping Chen Lin <yanpingchenlin@gmail.com>
2025-06-27 11:09:37 +0200

    [e2e] Update to cypress 14.5, and add return after Cypress.stop() and check before

--- apps/frontend-e2e/src/support/index.ts
+++ apps/frontend-e2e/src/support/index.ts
@@ -1,7 +1,11 @@
 import { Context } from 'mocha';
 
+// eslint-disable-next-line func-names
 afterEach(function (this: Context) {
   if (this.currentTest?.state === 'failed') {
+    Cypress.env('cancelled', 'cancelled');
     Cypress.stop();
+    // eslint-disable-next-line no-useless-return
+    return;
   }
 });
