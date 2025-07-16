3933b1797ac2955dfec40b4bd36ad11c66ba4d78
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2025-07-16 16:09:36 +0200

    Show comment item connections in review comments

--- apps/api/src/app/controllers/review-unit-comment.controller.ts
+++ apps/api/src/app/controllers/review-unit-comment.controller.ts
@@ -3,18 +3,31 @@ import {
   Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards
 } from '@nestjs/common';
 import {
-  ApiUnauthorizedResponse, ApiBearerAuth, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiTags
+  ApiUnauthorizedResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiTags,
+  ApiParam,
+  ApiInternalServerErrorResponse
 } from '@nestjs/swagger';
 import {
-  CreateUnitCommentDto, UnitCommentDto, UpdateUnitCommentDto
+  CreateUnitCommentDto, UnitCommentDto, UpdateUnitCommentDto, UpdateUnitCommentUnitItemsDto
 } from '@studio-lite-lib/api-dto';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { UnitCommentService } from '../services/unit-comment.service';
+import { WorkspaceGuard } from '../guards/workspace.guard';
+import { CommentAccessGuard } from '../guards/comment-access.guard';
+import { CommentWriteGuard } from '../guards/comment-write.guard';
+import { UnitId } from '../decorators/unit-id.decorator';
+import { ItemCommentService } from '../services/item-comment.service';
 
 @Controller('reviews/:review_id/units')
 export class ReviewUnitCommentController {
   constructor(
-    private unitCommentService: UnitCommentService
+    private unitCommentService: UnitCommentService,
+    private itemCommentService: ItemCommentService
   ) {}
 
   @Get(':id/comments')
@@ -60,5 +73,21 @@ export class ReviewUnitCommentController {
   @ApiTags('review unit comment')
   async removeComment(@Param('id', ParseIntPipe) id: number) {
     return this.unitCommentService.removeComment(id);
+  }
+
+  @Patch(':unit_id/comments/:comment_id/items')
+  @UseGuards(JwtAuthGuard, WorkspaceGuard, CommentAccessGuard, CommentWriteGuard)
+  @ApiBearerAuth()
+  @ApiParam({ name: 'review_id', type: Number })
+  @ApiParam({ name: 'unit_id', type: Number })
+  @ApiParam({ name: 'comment_id', type: Number })
+  @ApiOkResponse({ description: 'Comment item connections for successfully updated.' })
+  @ApiNotFoundResponse({ description: 'Comment not found.' })
+  @ApiUnauthorizedResponse({ description: 'Not authorized to update comment.' })
+  @ApiInternalServerErrorResponse({ description: 'Internal error. ' })
+  async patchCommentItems(@Param('comment_id', ParseIntPipe) commentId: number,
+    @UnitId() unitId: number,
+    @Body() comment: UpdateUnitCommentUnitItemsDto) {
+    return this.itemCommentService.updateCommentItems(unitId, commentId, comment.unitItemUuids);
   }
 }
