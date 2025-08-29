63566d9e733e95ab4c04a926f4b462fda3a35b0b
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2025-08-29 13:08:05 +0200

    Redesign app layout

--- apps/frontend/src/app/components/app-info/app-info.component.ts
+++ apps/frontend/src/app/components/app-info/app-info.component.ts
@@ -2,7 +2,7 @@ import { Component, Input } from '@angular/core';
 import { SafeUrl } from '@angular/platform-browser';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterLink } from '@angular/router';
-import { MatAnchor, MatFabButton } from '@angular/material/button';
+import { MatAnchor, MatButton } from '@angular/material/button';
 
 import { AreaTitleComponent } from '../area-title/area-title.component';
 import { AppInfoContentComponent } from '../app-info-content/app-info-content.component';
@@ -11,7 +11,7 @@ import { AppInfoContentComponent } from '../app-info-content/app-info-content.co
   selector: 'studio-lite-app-info',
   templateUrl: './app-info.component.html',
   styleUrls: ['./app-info.component.scss'],
-  imports: [AreaTitleComponent, MatAnchor, RouterLink, TranslateModule, AppInfoContentComponent, MatFabButton]
+  imports: [AreaTitleComponent, MatAnchor, RouterLink, TranslateModule, AppInfoContentComponent, MatButton]
 })
 export class AppInfoComponent {
   @Input() appTitle!: string;
