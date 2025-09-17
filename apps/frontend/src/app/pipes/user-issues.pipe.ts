24fce8d6915c9c0c9a29f4aac0a48d277d6bdea4
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2025-09-17 13:05:47 +0200

    Simplify UserIssuesComponent, as it is no longer needed for reviews

--- apps/frontend/src/app/pipes/user-issues.pipe.ts
+++ apps/frontend/src/app/pipes/user-issues.pipe.ts
@@ -1,6 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { ReviewDto, WorkspaceDto } from '@studio-lite-lib/api-dto';
-import { TranslateService } from '@ngx-translate/core';
+import { WorkspaceDto } from '@studio-lite-lib/api-dto';
 import { UserIssue } from '../models/user-issue.interface';
 
 @Pipe({
@@ -8,17 +7,12 @@ import { UserIssue } from '../models/user-issue.interface';
   standalone: true
 })
 export class UserIssuesPipe implements PipeTransform {
-  constructor(private translateService: TranslateService) {}
-
-  transform(issues: Partial<ReviewDto>[] |
-  (Partial<WorkspaceDto> & { workspaceName: string; workspaceGroupName: string })[],
+  // eslint-disable-next-line class-methods-use-this
+  transform(issues: WorkspaceDto[],
             linkPrefix: string): UserIssue[] {
     return issues?.map(issue => ({
       link: `/${linkPrefix}/${issue.id}`,
-      name: issue.name as string,
-      toolTip: issue.workspaceName && issue.workspaceGroupName ?
-        this.translateService
-          .instant('review.info', { workspace: issue.workspaceName, workspaceGroup: issue.workspaceGroupName }) : ''
+      name: issue.name as string
     }));
   }
 }
