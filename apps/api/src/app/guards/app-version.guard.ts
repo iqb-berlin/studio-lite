19c27b6b194e98c9f19be4ee86580443e5156fd4
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2025-11-25 14:20:22 +0100

    Raise version to 13.7.0

--- apps/api/src/app/guards/app-version.guard.ts
+++ apps/api/src/app/guards/app-version.guard.ts
@@ -27,5 +27,5 @@ export class AppVersionGuard implements CanActivate {
 
 export const AppVersionProvider = {
   provide: 'APP_VERSION',
-  useValue: '13.6.0'
+  useValue: '13.7.0'
 };
--- apps/frontend-e2e/cypress.config.ts
+++ apps/frontend-e2e/cypress.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   env: {
     username: 'fadmin',
     password: '4445',
-    version: '13.6.0'
+    version: '13.7.0'
   },
   e2e: {
     ...nxE2EPreset(__dirname),
