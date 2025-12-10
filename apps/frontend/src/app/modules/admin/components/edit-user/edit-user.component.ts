dc036659704c305c7174da6ae1c7668c0b515ff7
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2025-12-10 14:50:48 +0100

    Open email client and send manipulated email template to new user

--- apps/frontend/src/app/modules/admin/components/edit-user/edit-user.component.ts
+++ apps/frontend/src/app/modules/admin/components/edit-user/edit-user.component.ts
@@ -6,13 +6,15 @@ import {
   UntypedFormBuilder, UntypedFormGroup, Validators, FormsModule, ReactiveFormsModule
 } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
-import { MatButton } from '@angular/material/button';
+import { MatButton, MatMiniFabButton } from '@angular/material/button';
 import { MatCheckbox } from '@angular/material/checkbox';
 import { MatIcon } from '@angular/material/icon';
 
 import { MatInput } from '@angular/material/input';
 import { MatFormField, MatLabel, MatSuffix } from '@angular/material/form-field';
+import { MatTooltip } from '@angular/material/tooltip';
 import { EditUserComponentData } from '../../models/edit-user-component-data.type';
+import { AppService } from '../../../../services/app.service';
 
 @Component({
   selector: 'studio-lite-edit-user',
@@ -20,13 +22,14 @@ import { EditUserComponentData } from '../../models/edit-user-component-data.typ
   styleUrls: ['./edit-user.component.scss'],
   imports: [MatDialogTitle, MatDialogContent, FormsModule, ReactiveFormsModule,
     MatFormField, MatInput, MatIcon, MatLabel, MatCheckbox, MatDialogActions, MatSuffix,
-    MatButton, MatDialogClose, TranslateModule]
+    MatButton, MatDialogClose, TranslateModule, MatTooltip, MatMiniFabButton]
 })
 
 export class EditUserComponent {
   editUserForm: UntypedFormGroup;
   constructor(private fb: UntypedFormBuilder,
-              @Inject(MAT_DIALOG_DATA) public data: EditUserComponentData) {
+              @Inject(MAT_DIALOG_DATA) public data: EditUserComponentData,
+              private appService: AppService) {
     this.editUserForm = this.fb.group({
       name: this.fb.control(this.data.name,
         [Validators.required, Validators.pattern(/^[a-zäöüß]{3,}$/)]),
@@ -54,33 +57,44 @@ export class EditUserComponent {
   }
 
   private static generatePassword(length = 8): string {
-    const CHARSET =
-      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&*-_+=;:,.<>?`';
-
-    let cryptoObj: Crypto | undefined;
-
-    if (typeof crypto !== 'undefined') {
-      cryptoObj = crypto;
-    } else if (typeof globalThis !== 'undefined') {
-      const maybeCrypto = (globalThis as unknown as { crypto?: unknown }).crypto;
-      if (maybeCrypto instanceof Crypto) {
-        cryptoObj = maybeCrypto;
-      }
-    }
-
-    if (!cryptoObj || typeof cryptoObj.getRandomValues !== 'function') {
-      return Array.from({ length }, () => CHARSET.charAt(Math.floor(Math.random() * CHARSET.length))
-      ).join('');
-    }
+    const CHARSET = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789!#$%&*-_+=:?';
     return Array.from({ length }, () => {
       const buf = new Uint8Array(1);
       const maxValid = 256 - (256 % CHARSET.length);
       let rnd = 0;
       do {
-        cryptoObj.getRandomValues(buf);
+        crypto.getRandomValues(buf);
         rnd = buf[0];
       } while (rnd >= maxValid);
       return CHARSET.charAt(rnd % CHARSET.length);
     }).join('');
+  }
+
+  mailTo(): void {
+    const email: string = this.editUserForm.get('email')?.value || '';
+    const subject: string = 'Zugang';
+    // console.log(this.appService.appConfig.emailText);
+    window.location.href = `mailto:${email}?subject=${subject}&body=${this.getEmailBody()}`;
+  }
+
+  private getEmailBody(): string {
+    const template = this.appService.appConfig.emailText;
+    const firstName = this.editUserForm.get('firstName')?.value || '';
+    const lastName = this.editUserForm.get('lastName')?.value || '';
+    const name = this.editUserForm.get('name')?.value || '';
+    const password = this.editUserForm.get('password')?.value || '';
+
+    const result = EditUserComponent.fillTemplate(template, {
+      firstname: firstName,
+      lastname: lastName,
+      user: name,
+      password: password
+    });
+    return encodeURIComponent(result);
+  }
+
+  private static fillTemplate(template: string, data: { [key: string]: string }): string {
+    return template
+      .replace(/{{(.*?)}}/g, (_, key) => data[key.trim()] ?? '');
   }
 }
