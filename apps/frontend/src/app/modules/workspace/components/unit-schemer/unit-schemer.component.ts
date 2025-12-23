ce691daba767d00617891696e1d72ac92a4c10bc
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2025-12-23 11:14:30 +0100

    Apply changes from UnitEditComponent to UnitSchemerComponent

--- apps/frontend/src/app/modules/workspace/components/unit-schemer/unit-schemer.component.ts
+++ apps/frontend/src/app/modules/workspace/components/unit-schemer/unit-schemer.component.ts
@@ -1,7 +1,9 @@
 import {
   AfterViewInit, Component, ElementRef, OnDestroy, ViewChild
 } from '@angular/core';
-import { Subject, Subscription, takeUntil } from 'rxjs';
+import {
+  BehaviorSubject, from, map, Observable, of, Subject, takeUntil
+} from 'rxjs';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { VeronaModuleFactory } from '@studio-lite/shared-code';
 import { TranslateService } from '@ngx-translate/core';
@@ -12,24 +14,30 @@ import { UnitSchemeStore } from '../../classes/unit-scheme-store';
 import { ModuleService } from '../../../shared/services/module.service';
 import { SubscribeUnitDefinitionChangesDirective } from '../../directives/subscribe-unit-definition-changes.directive';
 import { RolePipe } from '../../pipes/role.pipe';
+import { UnitMetadataStore } from '../../classes/unit-metadata-store';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'studio-lite-unit-schemer',
   templateUrl: './unit-schemer.component.html',
   styleUrls: ['./unit-schemer.component.scss'],
   host: { class: 'unit-schemer' },
-  imports: []
+  imports: [
+    MatProgressSpinner
+  ]
 })
-export class UnitSchemerComponent extends SubscribeUnitDefinitionChangesDirective implements AfterViewInit, OnDestroy {
+export class UnitSchemerComponent
+  extends SubscribeUnitDefinitionChangesDirective
+  implements AfterViewInit, OnDestroy {
   @ViewChild('hostingIframe') hostingIframe!: ElementRef;
-  private readonly postMessageSubscription: Subscription;
-  private unitIdChangedSubscription: Subscription | undefined;
   private iFrameElement: HTMLIFrameElement | undefined;
   private postMessageTarget: Window | undefined;
   private sessionId = '';
   private lastSchemerId = '';
   ngUnsubscribe = new Subject<void>();
   message = '';
+  unitLoaded: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(true);
+  loading = false;
 
   constructor(
     private backendService: WorkspaceBackendService,
@@ -40,131 +48,221 @@ export class UnitSchemerComponent extends SubscribeUnitDefinitionChangesDirectiv
     private translateService: TranslateService
   ) {
     super();
-    this.postMessageSubscription = this.appService.postMessage$.subscribe((m: MessageEvent) => {
-      const msgData = m.data;
-      const msgType = msgData.type;
-
-      if ((msgType !== undefined) && (msgType !== null) && (m.source === this.iFrameElement?.contentWindow)) {
-        this.postMessageTarget = m.source as Window;
-        switch (msgType) {
-          case 'vosReadyNotification':
-            this.sessionId = Math.floor(Math.random() * 20000000 + 10000000).toString();
-            this.postMessageTarget = m.source as Window;
-            this.sendUnitData();
-            break;
-
-          case 'vosSchemeChangedNotification':
-            if (msgData.sessionId === this.sessionId) {
-              if (msgData.codingScheme) {
-                this.workspaceService.codingScheme = JSON.parse(msgData.codingScheme);
-                this.workspaceService.getUnitSchemeStore()?.setData(msgData.codingScheme, msgData.codingSchemeType);
-                // } else { TODO: find solution for vosGetSchemeRequest
-                //   this.postMessageTarget.postMessage({
-                //     type: 'vosGetSchemeRequest',
-                //     sessionId: this.sessionId
-                //   }, '*');
-              }
-            }
-            break;
-
-          default:
-            // eslint-disable-next-line no-console
-            console.warn(`processMessagePost ignored message: ${msgType}`);
-            break;
-        }
-      }
-    });
+    this.unitLoaded.subscribe(loaded => setTimeout(() => {
+      this.loading = !loaded;
+    }));
   }
 
   ngAfterViewInit(): void {
     this.iFrameElement = this.hostingIframe.nativeElement;
-    this.unitIdChangedSubscription = this.workspaceService.selectedUnit$.subscribe(() => {
-      this.message = '';
-      this.workspaceService
-        .loadUnitProperties()
-        .pipe(takeUntil(this.ngUnsubscribe))
-        .subscribe(() => this.sendUnitData());
-    });
+    this.subscribeForPostMessages();
+    this.subscribeForSelectedUnitChange();
     this.addSubscriptionForUnitDefinitionChanges();
   }
 
-  async sendUnitData() {
-    const unitId = this.workspaceService.selectedUnit$.getValue();
-    const unitMetadataStore = this.workspaceService.getUnitMetadataStore();
-    if (unitId && unitId > 0 && unitMetadataStore) {
-      const unitMetadata = unitMetadataStore.getData();
-      if (Object.keys(this.moduleService.schemers).length === 0) await this.moduleService.loadList();
-      const schemerId = unitMetadata.schemer ?
-        VeronaModuleFactory.getBestMatch(unitMetadata.schemer, Object.keys(this.moduleService.schemers)) : '';
-      if (schemerId) {
-        if ((schemerId === this.lastSchemerId) && this.postMessageTarget) {
-          let unitSchemeStore = this.workspaceService.getUnitSchemeStore();
-          if (unitSchemeStore) {
-            this.postUnitScheme(unitSchemeStore);
-          } else {
-            this.backendService.getUnitScheme(this.workspaceService.selectedWorkspaceId, unitId).subscribe(
-              ues => {
-                if (ues) {
-                  unitSchemeStore = new UnitSchemeStore(unitId, ues);
-                  this.workspaceService.setUnitSchemeStore(unitSchemeStore);
-                  this.postUnitScheme(unitSchemeStore);
-                } else {
-                  this.snackBar.open(
-                    this.translateService.instant('workspace.coding-scheme-not-loaded'),
-                    this.translateService.instant('workspace.error'),
-                    { duration: 3000 });
+  private subscribeForSelectedUnitChange(): void {
+    this.workspaceService.selectedUnit$
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(() => {
+        if (this.unitLoaded.getValue()) {
+          this.unitLoaded.next(false);
+          this.message = '';
+          this.workspaceService
+            .loadUnitProperties()
+            .pipe(takeUntil(this.ngUnsubscribe))
+            .subscribe(() => this.sendUnitData());
+        } else {
+          this.ngUnsubscribe.next();
+          this.ngUnsubscribe.complete();
+          this.ngUnsubscribe = new Subject<void>();
+          this.unitLoaded.next(true);
+          this.subscribeForPostMessages();
+          this.subscribeForSelectedUnitChange();
+          this.addSubscriptionForUnitDefinitionChanges();
+        }
+      });
+  }
+
+  private subscribeForPostMessages(): void {
+    this.appService.postMessage$
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((m: MessageEvent) => {
+        const msgData = m.data;
+        const msgType = msgData.type;
+
+        if (
+          msgType !== undefined &&
+          msgType !== null &&
+          m.source === this.iFrameElement?.contentWindow
+        ) {
+          this.postMessageTarget = m.source as Window;
+          switch (msgType) {
+            case 'vosReadyNotification':
+              this.sessionId = Math.floor(
+                Math.random() * 20000000 + 10000000
+              ).toString();
+              this.postMessageTarget = m.source as Window;
+              this.sendScheme(
+                this.workspaceService.selectedUnit$.getValue(),
+                this.workspaceService.getUnitSchemeStore()
+              );
+              break;
+
+            case 'vosSchemeChangedNotification':
+              if (msgData.sessionId === this.sessionId) {
+                if (msgData.codingScheme) {
+                  this.workspaceService.codingScheme = JSON.parse(
+                    msgData.codingScheme
+                  );
+                  this.workspaceService
+                    .getUnitSchemeStore()
+                    ?.setData(msgData.codingScheme, msgData.codingSchemeType);
+                  // } else { TODO: find solution for vosGetSchemeRequest
+                  //   this.postMessageTarget.postMessage({
+                  //     type: 'vosGetSchemeRequest',
+                  //     sessionId: this.sessionId
+                  //   }, '*');
                 }
               }
+              break;
+
+            default:
+              // eslint-disable-next-line no-console
+              console.warn(`processMessagePost ignored message: ${msgType}`);
+              break;
+          }
+        }
+      });
+  }
+
+  private getSchemerId(
+    unitMetadataStore: UnitMetadataStore | undefined
+  ): Observable<string> {
+    if (unitMetadataStore) {
+      const unitMetadata = unitMetadataStore.getData();
+
+      const loadList$ =
+        Object.keys(this.moduleService.schemers).length === 0 ?
+          from(this.moduleService.loadList()) :
+          of(undefined);
+
+      return loadList$.pipe(
+        map(() => (unitMetadata.schemer ?
+          VeronaModuleFactory.getBestMatch(
+            unitMetadata.schemer,
+            Object.keys(this.moduleService.schemers)
+          ) :
+          '')
+        )
+      );
+    }
+    return of('');
+  }
+
+  private sendScheme(
+    unitId: number,
+    unitSchemeStore: UnitSchemeStore | undefined
+  ) {
+    if (!unitId) {
+      this.message = this.translateService.instant('workspace.unit-not-found');
+      this.postMessageTarget = undefined;
+      return;
+    }
+    if (unitSchemeStore) {
+      this.postUnitScheme(unitSchemeStore);
+    } else {
+      this.backendService
+        .getUnitScheme(this.workspaceService.selectedWorkspaceId, unitId)
+        .subscribe(ues => {
+          if (ues) {
+            const newUnitSchemeStore = new UnitSchemeStore(unitId, ues);
+            this.workspaceService.setUnitSchemeStore(newUnitSchemeStore);
+            this.postUnitScheme(newUnitSchemeStore);
+          } else {
+            this.snackBar.open(
+              this.translateService.instant(
+                'workspace.coding-scheme-not-loaded'
+              ),
+              this.translateService.instant('workspace.error'),
+              { duration: 3000 }
             );
           }
-        } else {
-          this.buildSchemer(schemerId);
-          // schemer gets unit data via ReadyNotification
-        }
-      } else {
-        this.message = this.translateService.instant('workspace.no-schemer');
-        this.buildSchemer();
-      }
-    } else {
-      this.message = this.translateService.instant('workspace.unit-not-found');
-      this.buildSchemer();
+        });
     }
+  }
+
+  sendUnitData() {
+    this.getSchemerId(this.workspaceService.getUnitMetadataStore())
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(schemerId => {
+        if (schemerId) {
+          if (schemerId === this.lastSchemerId && this.postMessageTarget) {
+            this.sendScheme(
+              this.workspaceService.selectedUnit$.getValue(),
+              this.workspaceService.getUnitSchemeStore()
+            );
+          } else {
+            this.postMessageTarget = undefined;
+            this.buildSchemer(schemerId);
+            // schemer gets unit data via ReadyNotification
+          }
+        } else {
+          this.postMessageTarget = undefined;
+          this.message = this.translateService.instant(
+            'workspace.no-schemer'
+          );
+        }
+      });
   }
 
   private postUnitScheme(unitSchemeStore: UnitSchemeStore): void {
     const unitScheme = unitSchemeStore.getData();
-    const variables = this.workspaceService.getUnitDefinitionStore()?.getData()
-      .variables || unitScheme.variables;
+    const variables =
+      this.workspaceService.getUnitDefinitionStore()?.getData().variables ||
+      unitScheme.variables;
     if (this.postMessageTarget) {
-      this.postMessageTarget.postMessage({
-        type: 'vosStartCommand',
-        sessionId: this.sessionId,
-        schemerConfig: {
-          definitionReportPolicy: 'eager',
-          role: new RolePipe().transform(this.workspaceService.userAccessLevel)
+      this.postMessageTarget.postMessage(
+        {
+          type: 'vosStartCommand',
+          sessionId: this.sessionId,
+          schemerConfig: {
+            definitionReportPolicy: 'eager',
+            role: new RolePipe().transform(
+              this.workspaceService.userAccessLevel
+            )
+          },
+          codingScheme: unitScheme.scheme || '',
+          codingSchemeType: unitScheme.schemeType || '',
+          variables: variables || []
         },
-        codingScheme: unitScheme.scheme || '',
-        codingSchemeType: unitScheme.schemeType || '',
-        variables: variables || []
-      }, '*');
+        '*'
+      );
+      this.unitLoaded.next(true);
     }
   }
 
   private buildSchemer(schemerId?: string) {
-    this.postMessageTarget = undefined;
     if (this.iFrameElement) {
       this.iFrameElement.srcdoc = '';
       if (schemerId) {
-        this.moduleService.getModuleHtml(this.moduleService.schemers[schemerId]).then(schemerData => {
-          if (schemerData) {
-            this.setupSchemerIFrame(schemerData);
-            this.lastSchemerId = schemerId;
-          } else {
-            this.message = this.translateService
-              .instant('workspace.schemer-not-loaded', { id: schemerId });
-            this.lastSchemerId = '';
-          }
-        });
+        from(
+          this.moduleService.getModuleHtml(
+            this.moduleService.schemers[schemerId]
+          )
+        )
+          .pipe(takeUntil(this.ngUnsubscribe))
+          .subscribe(schemerData => {
+            if (schemerData) {
+              this.setupSchemerIFrame(schemerData);
+              this.lastSchemerId = schemerId;
+            } else {
+              this.message = this.translateService.instant(
+                'workspace.schemer-not-loaded',
+                { id: schemerId }
+              );
+              this.lastSchemerId = '';
+            }
+          });
       } else {
         this.lastSchemerId = '';
       }
@@ -178,9 +276,8 @@ export class UnitSchemerComponent extends SubscribeUnitDefinitionChangesDirectiv
   }
 
   ngOnDestroy(): void {
-    if (this.unitIdChangedSubscription) this.unitIdChangedSubscription.unsubscribe();
-    if (this.postMessageSubscription !== null) this.postMessageSubscription.unsubscribe();
     this.ngUnsubscribe.next();
     this.ngUnsubscribe.complete();
+    this.unitLoaded.complete();
   }
 }
