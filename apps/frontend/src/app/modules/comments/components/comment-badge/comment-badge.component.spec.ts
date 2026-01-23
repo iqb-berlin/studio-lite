5163b89d1e52b1dabe1cb5d13f36bd928b2e3fb8
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2026-01-23 14:23:27 +0100

    Add unit tests for CommentBadgeComponent

--- apps/frontend/src/app/modules/comments/components/comment-badge/comment-badge.component.spec.ts
+++ apps/frontend/src/app/modules/comments/components/comment-badge/comment-badge.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatTooltip, MatTooltipModule } from '@angular/material/tooltip';
+import { By } from '@angular/platform-browser';
 import { CommentBadgeComponent } from './comment-badge.component';
 
 describe('CommentBadgeComponent', () => {
@@ -19,5 +20,44 @@ describe('CommentBadgeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display acronym for "Lastname, Firstname"', () => {
+    component.userName = 'Doe, John';
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(component.userAcronym).toBe('JD');
+    const debugElement = fixture.debugElement.nativeElement.querySelector('.comment-acronym');
+    expect(debugElement.textContent.trim()).toBe('JD');
+  });
+
+  it('should display acronym for single name', () => {
+    component.userName = 'John';
+    component.ngOnInit();
+    fixture.detectChanges();
+    expect(component.userAcronym).toBe('JO');
+    const debugElement = fixture.debugElement.nativeElement.querySelector('.comment-acronym');
+    expect(debugElement.textContent.trim()).toBe('JO');
+  });
+
+  it('should apply "own-comment" class when ownComment is true', () => {
+    component.ownComment = true;
+    fixture.detectChanges();
+    const debugElement = fixture.debugElement.nativeElement.querySelector('.comment-acronym');
+    expect(debugElement.classList.contains('own-comment')).toBe(true);
+  });
+
+  it('should not apply "own-comment" class when ownComment is false', () => {
+    component.ownComment = false;
+    fixture.detectChanges();
+    const debugElement = fixture.debugElement.nativeElement.querySelector('.comment-acronym');
+    expect(debugElement.classList.contains('own-comment')).toBe(false);
+  });
+
+  it('should have userName as tooltip', () => {
+    component.userName = 'Doe, John';
+    fixture.detectChanges();
+    const tooltipDirective = fixture.debugElement.query(By.directive(MatTooltip)).injector.get(MatTooltip);
+    expect(tooltipDirective.message).toBe('Doe, John');
   });
 });
