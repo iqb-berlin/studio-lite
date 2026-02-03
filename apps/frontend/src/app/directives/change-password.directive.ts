f21ff9ef712574dc400a22c971a457bafb2cb88e
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2026-02-03 13:03:06 +0100

    Add unsubscriptions

--- apps/frontend/src/app/directives/change-password.directive.ts
+++ apps/frontend/src/app/directives/change-password.directive.ts
@@ -1,7 +1,8 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, OnDestroy } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { TranslateService } from '@ngx-translate/core';
+import { Subject, takeUntil } from 'rxjs';
 import { BackendService } from '../services/backend.service';
 import { ChangePasswordComponent } from '../components/change-password/change-password.component';
 
@@ -9,7 +10,9 @@ import { ChangePasswordComponent } from '../components/change-password/change-pa
   selector: '[studioLiteChangePassword]',
   standalone: true
 })
-export class ChangePasswordDirective {
+export class ChangePasswordDirective implements OnDestroy {
+  private ngUnsubscribe = new Subject<void>();
+
   constructor(
     private changePasswordDialog: MatDialog,
     private backendService: BackendService,
@@ -18,28 +21,37 @@ export class ChangePasswordDirective {
   ) {
   }
 
+  ngOnDestroy(): void {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
+  }
+
   @HostListener('click')
   changePassword() {
     const dialogRef = this.changePasswordDialog.open(ChangePasswordComponent, {
       width: '400px'
     });
 
-    dialogRef.afterClosed().subscribe(result => {
-      if (result !== false) {
-        this.backendService.setUserPassword(result.controls.pw_old.value, result.controls.pw_new1.value).subscribe(
-          respOk => {
-            this.snackBar.open(
-              respOk ?
-                this.translateService.instant('user-profile.new-password') :
-                this.translateService.instant('user-profile.new-password-error'),
-              respOk ?
-                this.translateService.instant('user-profile.ok') :
-                this.translateService.instant('user-profile.error'),
-              { duration: 3000 }
+    dialogRef.afterClosed()
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe(result => {
+        if (result !== false) {
+          this.backendService.setUserPassword(result.controls.pw_old.value, result.controls.pw_new1.value)
+            .pipe(takeUntil(this.ngUnsubscribe))
+            .subscribe(
+              respOk => {
+                this.snackBar.open(
+                  respOk ?
+                    this.translateService.instant('user-profile.new-password') :
+                    this.translateService.instant('user-profile.new-password-error'),
+                  respOk ?
+                    this.translateService.instant('user-profile.ok') :
+                    this.translateService.instant('user-profile.error'),
+                  { duration: 3000 }
+                );
+              }
             );
-          }
-        );
-      }
-    });
+        }
+      });
   }
 }
