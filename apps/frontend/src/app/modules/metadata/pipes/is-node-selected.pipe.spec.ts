a3420260e64b329cb1e5c1cbea54102f1e10d7e4
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2026-02-06 15:25:07 +0100

    Add unit tests for pipes in metadata module

--- apps/frontend/src/app/modules/metadata/pipes/is-node-selected.pipe.spec.ts
+++ apps/frontend/src/app/modules/metadata/pipes/is-node-selected.pipe.spec.ts
@@ -1,7 +1,33 @@
+import { SelectionModel } from '@angular/cdk/collections';
 import { IsNodeSelectedPipe } from './is-node-selected.pipe';
+import { VocabFlatNode } from '../models/vocabulary.class';
 
 describe('IsNodeSelectedPipe', () => {
+  let pipe: IsNodeSelectedPipe;
+  let selectionModel: SelectionModel<VocabFlatNode>;
+
+  beforeEach(() => {
+    pipe = new IsNodeSelectedPipe();
+    selectionModel = new SelectionModel<VocabFlatNode>(true);
+  });
+
   it('should be defined', () => {
-    expect(new IsNodeSelectedPipe()).toBeDefined();
+    expect(pipe).toBeDefined();
+  });
+
+  it('should return true if node is selected', () => {
+    const node = { id: '1' } as VocabFlatNode;
+    jest.spyOn(selectionModel, 'isSelected').mockReturnValue(true);
+
+    expect(pipe.transform(selectionModel, node, false)).toBe(true);
+    expect(selectionModel.isSelected).toHaveBeenCalledWith(node);
+  });
+
+  it('should return false if node is not selected', () => {
+    const node = { id: '1' } as VocabFlatNode;
+    jest.spyOn(selectionModel, 'isSelected').mockReturnValue(false);
+
+    expect(pipe.transform(selectionModel, node, false)).toBe(false);
+    expect(selectionModel.isSelected).toHaveBeenCalledWith(node);
   });
 });
