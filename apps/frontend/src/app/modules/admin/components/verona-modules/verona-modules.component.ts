85e0ef03e45925b429d46e64491db90cdf6f2321
jojohoch <joachim.hoch@iqb.hu-berlin.de>
2026-02-12 17:00:54 +0100

    Extract common logic for verona modules to a base class

--- apps/frontend/src/app/modules/admin/components/verona-modules/verona-modules.component.ts
+++ apps/frontend/src/app/modules/admin/components/verona-modules/verona-modules.component.ts
@@ -1,5 +1,5 @@
 import {
-  Component, OnInit, Inject, ViewChildren, QueryList
+  Component, Inject
 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -7,14 +7,14 @@ import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { MatTooltip } from '@angular/material/tooltip';
 import { MatButton } from '@angular/material/button';
 import {
-  IqbFilesUploadInputForDirective, IqbFilesUploadQueueComponent, ConfirmDialogComponent, ConfirmDialogData
+  IqbFilesUploadInputForDirective, IqbFilesUploadQueueComponent
 } from '@studio-lite-lib/iqb-components';
 import { ModuleService } from '../../../shared/services/module.service';
 import { BackendService } from '../../services/backend.service';
 import { AppService } from '../../../../services/app.service';
 import { VeronaModulesTableComponent } from '../verona-modules-table/verona-modules-table.component';
-import { VeronaModuleClass } from '../../../shared/models/verona-module.class';
 import { WrappedIconComponent } from '../../../shared/components/wrapped-icon/wrapped-icon.component';
+import { ModulesDirective } from '../../directives/modules.directive';
 
 @Component({
   selector: 'studio-lite-verona-modules',
@@ -23,92 +23,48 @@ import { WrappedIconComponent } from '../../../shared/components/wrapped-icon/wr
   // eslint-disable-next-line max-len
   imports: [MatButton, MatTooltip, WrappedIconComponent, IqbFilesUploadInputForDirective, IqbFilesUploadQueueComponent, VeronaModulesTableComponent, TranslateModule]
 })
-export class VeronaModulesComponent implements OnInit {
-  @ViewChildren(VeronaModulesTableComponent) moduleTables!: QueryList<VeronaModulesTableComponent>;
-  selectedModules: VeronaModuleClass[] = [];
-  uploadUrl = '';
-  token: string | undefined;
+export class VeronaModulesComponent extends ModulesDirective {
+  protected readonly pageTitleKey = 'modules.title';
+  protected readonly uploadPath = 'admin/verona-modules';
 
-  constructor(
-    @Inject('SERVER_URL') public serverUrl: string,
-    private appService: AppService,
-    public moduleService: ModuleService,
-    private backendService: BackendService,
-    private deleteConfirmDialog: MatDialog,
-    private snackBar: MatSnackBar,
-    private translateService: TranslateService
-  ) {
-    this.uploadUrl = `${this.serverUrl}admin/verona-modules`;
+  protected serverUrl: string;
+  protected appService: AppService;
+  moduleService: ModuleService;
+  protected backendService: BackendService;
+  protected deleteConfirmDialog: MatDialog;
+  protected snackBar: MatSnackBar;
+  protected translateService: TranslateService;
+
+  constructor(@Inject('SERVER_URL') serverUrl: string,
+                                    appService: AppService,
+                                    moduleService: ModuleService,
+                                    backendService: BackendService,
+                                    deleteConfirmDialog: MatDialog,
+                                    snackBar: MatSnackBar,
+                                    translateService: TranslateService) {
+    super();
+    this.serverUrl = serverUrl;
+    this.appService = appService;
+    this.moduleService = moduleService;
+    this.backendService = backendService;
+    this.deleteConfirmDialog = deleteConfirmDialog;
+    this.snackBar = snackBar;
+    this.translateService = translateService;
+    this.updateUploadUrl();
   }
 
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.appService.appConfig
-        .setPageTitle(this.translateService.instant('modules.title'));
-      const token = localStorage.getItem('t');
-      this.loadModuleList();
-      this.token = token || '';
-    });
+  protected getPageTitleKey(): string {
+    return this.pageTitleKey;
+  }
+
+  protected getUploadPath(): string {
+    return this.uploadPath;
   }
 
   loadModuleList() {
     this.appService.dataLoading = true;
     this.moduleService.loadList().then(() => {
       this.appService.dataLoading = false;
-    });
-  }
-
-  changeSelectedModules(selection: { type: string; selectedModules: VeronaModuleClass[] }) {
-    const newSelection: VeronaModuleClass[] = [];
-    this.selectedModules.forEach(m => {
-      if (m.metadata.type !== selection.type) {
-        newSelection.push(m);
-      }
-    });
-    selection.selectedModules.forEach(m => {
-      newSelection.push(m);
-    });
-    this.selectedModules = newSelection;
-  }
-
-  deleteFiles(): void {
-    const content = (this.selectedModules.length > 1) ?
-      this.translateService
-        .instant('modules.delete-modules', { count: this.selectedModules.length }) :
-      this.translateService
-        .instant('modules.delete-module');
-    const dialogRef = this.deleteConfirmDialog.open(ConfirmDialogComponent, {
-      width: '400px',
-      data: <ConfirmDialogData>{
-        title: this.translateService.instant('modules.delete-title'),
-        content: content,
-        confirmButtonLabel: this.translateService.instant('delete'),
-        showCancel: true
-      }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result !== false) {
-        this.appService.dataLoading = true;
-        this.backendService.deleteVeronaModules(this.selectedModules.map(element => element.key)).subscribe(
-          (deleteFilesOk: boolean) => {
-            if (deleteFilesOk) {
-              this.snackBar.open(
-                this.translateService
-                  .instant('modules.deleted', { count: this.selectedModules.length }),
-                '',
-                { duration: 1000 });
-              this.loadModuleList();
-            } else {
-              this.snackBar.open(
-                this.translateService.instant('modules.not-deleted'),
-                this.translateService.instant('error'),
-                { duration: 3000 });
-              this.appService.dataLoading = false;
-            }
-          }
-        );
-      }
     });
   }
 }
