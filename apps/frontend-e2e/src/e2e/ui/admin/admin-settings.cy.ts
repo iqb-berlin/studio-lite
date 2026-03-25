b47d627ebff5fde96edebf78daa7064a078f30ba
Yan Ping Chen Lin <yanpingchenlin@gmail.com>
2026-03-25 12:04:34 +0100

    [e2e] Add e2e test for registry, missing profile and unit export admin setting card

--- apps/frontend-e2e/src/e2e/ui/admin/admin-settings.cy.ts
+++ apps/frontend-e2e/src/e2e/ui/admin/admin-settings.cy.ts
@@ -6,8 +6,10 @@ import {
   saveAndExpect,
   setFormControl,
   logout,
-  login
+  login,
+  createGroup, deleteGroup
 } from '../../../support/helpers';
+import { group1 } from '../../../support/testData';
 
 describe('Admin Settings Tab Configuration', () => {
   before(() => addFirstUser());
@@ -74,7 +76,12 @@ describe('Admin Settings Tab Configuration', () => {
       clearFormControl('appTitle');
       clearFormControl('introHtml');
       clearFormControl('imprintHtml');
-      saveAndExpect('PATCH', '/api/admin/settings/config', 'saveConfigClean', 0);
+      saveAndExpect(
+        'PATCH',
+        '/api/admin/settings/config',
+        'saveConfigClean',
+        0
+      );
     });
   });
 
@@ -95,11 +102,18 @@ describe('Admin Settings Tab Configuration', () => {
     it('saves a custom application background color', () => {
       goToSettings();
       setFormControl('bodyBackground', '#f0f4f8');
-      saveAndExpect('PATCH', '/api/admin/settings/app-logo', 'saveBackground', 2);
+      saveAndExpect(
+        'PATCH',
+        '/api/admin/settings/app-logo',
+        'saveBackground',
+        2
+      );
       cy.reload();
       cy.window().should(win => {
-        const bodyBg = win.getComputedStyle(win.document.documentElement)
-          .getPropertyValue('--st-body-background').trim();
+        const bodyBg = win
+          .getComputedStyle(win.document.documentElement)
+          .getPropertyValue('--st-body-background')
+          .trim();
         expect(bodyBg).to.eq('#f0f4f8');
       });
     });
@@ -107,11 +121,18 @@ describe('Admin Settings Tab Configuration', () => {
     it('saves a custom box background color', () => {
       goToSettings();
       setFormControl('boxBackground', '#ffffff');
-      saveAndExpect('PATCH', '/api/admin/settings/app-logo', 'saveBoxBackground', 2);
+      saveAndExpect(
+        'PATCH',
+        '/api/admin/settings/app-logo',
+        'saveBoxBackground',
+        2
+      );
       cy.reload();
       cy.window().should(win => {
-        const boxBg = win.getComputedStyle(win.document.documentElement)
-          .getPropertyValue('--st-box-background').trim();
+        const boxBg = win
+          .getComputedStyle(win.document.documentElement)
+          .getPropertyValue('--st-box-background')
+          .trim();
         expect(boxBg).to.eq('#ffffff');
       });
     });
@@ -120,13 +141,158 @@ describe('Admin Settings Tab Configuration', () => {
       goToSettings();
       clearFormControl('bodyBackground');
       clearFormControl('boxBackground');
-      saveAndExpect('PATCH', '/api/admin/settings/app-logo', 'originalBackground', 2);
+      saveAndExpect(
+        'PATCH',
+        '/api/admin/settings/app-logo',
+        'originalBackground',
+        2
+      );
       cy.reload();
       cy.window().should(win => {
-        const bodyBg = win.getComputedStyle(win.document.documentElement)
-          .getPropertyValue('--st-body-background').trim();
+        const bodyBg = win
+          .getComputedStyle(win.document.documentElement)
+          .getPropertyValue('--st-body-background')
+          .trim();
         expect(bodyBg).not.be.eq('#f0f4f8');
       });
+    });
+  });
+
+  describe('Unit Export Config card', () => {
+    const unitXsdUrl =
+      'https://github.com/iqb-berlin/testcenter/blob/master/definitions/vo_Unit.xsd';
+    const bookletXsdUrl =
+      'https://github.com/iqb-berlin/testcenter/blob/master/definitions/vo_Booklet.xsd';
+    const testTakersXsdUrl =
+      'https://github.com/iqb-berlin/testcenter/blob/master/definitions/vo_Testtakers.xsd';
+
+    it('displays the Unit Export Config fields', () => {
+      goToSettings();
+      cy.get('[formcontrolname="unitXsdUrl"]').should('exist');
+      cy.get('[formcontrolname="bookletXsdUrl"]').should('exist');
+      cy.get('[formcontrolname="testTakersXsdUrl"]').should('exist');
+    });
+
+    it('clears the XSD URL fields', () => {
+      goToSettings();
+      clearFormControl('unitXsdUrl');
+      clearFormControl('bookletXsdUrl');
+      clearFormControl('testTakersXsdUrl');
+      saveAndExpect(
+        'PATCH',
+        '/api/admin/settings/unit-export-config',
+        'clearUnitExport',
+        3
+      );
+    });
+
+    it('saves the three XSD URL parameters', () => {
+      goToSettings();
+      setFormControl('unitXsdUrl', unitXsdUrl);
+      setFormControl('bookletXsdUrl', bookletXsdUrl);
+      setFormControl('testTakersXsdUrl', testTakersXsdUrl);
+      saveAndExpect(
+        'PATCH',
+        '/api/admin/settings/unit-export-config',
+        'saveUnitExport',
+        3
+      );
+    });
+  });
+
+  // -------------------------------------------------------------------------
+  describe('Profiles Registry card', () => {
+    const testUrl = 'https://raw.githubusercontent.com/iqb-vocabs/profile-registry/refs/heads/master/test-registry.csv';
+    const originalUrl =
+      'https://raw.githubusercontent.com/iqb-vocabs/profile-registry/master/registry.csv';
+    it('displays the Profiles Registry CSV URL field', () => {
+      goToSettings();
+      cy.get('[formcontrolname="csvUrl"]').should('exist');
+    });
+
+    it('clears the profiles registry URL', () => {
+      goToSettings();
+      clearFormControl('csvUrl');
+      saveAndExpect(
+        'PATCH',
+        '/api/admin/settings/profiles-registry',
+        'clearRegistry',
+        4
+      );
+    });
+
+    it('saves a profiles registry CSV URL', () => {
+      goToSettings();
+      setFormControl('csvUrl', testUrl);
+      saveAndExpect(
+        'PATCH',
+        '/api/admin/settings/profiles-registry',
+        'saveRegistry',
+        4
+      );
+    });
+
+    it('checks that the we have only two registry stores with the test registry', () => {
+      // create a group workspace
+      createGroup(group1);
+
+      // checks that we have only two profiles
+      cy.get('mat-table').contains(group1).click();
+      cy.get('[data-cy="workspaces-groups-menu-edit"]').click();
+      cy.get('studio-lite-profiles')
+        .get('mat-expansion-panel').should('have.length', 2);
+      cy.translate(Cypress.expose('locale')).then(json => {
+        cy.contains('button', json.cancel).click();
+      });
+
+      // deletes group
+      deleteGroup(group1);
+    });
+
+    it('restores the original profile registry CSV URL', () => {
+      goToSettings();
+      setFormControl('csvUrl', originalUrl);
+      saveAndExpect(
+        'PATCH',
+        '/api/admin/settings/profiles-registry',
+        'saveRegistry',
+        4
+      );
+    });
+  });
+
+  // -------------------------------------------------------------------------
+  describe('Missings Profiles card', () => {
+    const validJson =
+      '[{"id":"missing_by_intention","label":"missing by intention","description":"","code":-99}]';
+
+    it('displays the Missings Profiles IQB-Standard field', () => {
+      goToSettings();
+      cy.get('[formcontrolname="iqbStandardMissings"]').should('exist');
+    });
+
+    it('sets the original valid IQB-standard missings profile JSON', () => {
+      goToSettings();
+      clearFormControl('iqbStandardMissings');
+      cy.get('[formcontrolname="iqbStandardMissings"]')
+        .type(validJson, { parseSpecialCharSequences: false, delay: 0 });
+      saveAndExpect(
+        'PATCH',
+        '/api/admin/settings/missings-profiles',
+        'saveMissings',
+        5
+      );
+    });
+
+    it('sets an empty missings profile', () => {
+      goToSettings();
+      setFormControl('iqbStandardMissings', '[]');
+      saveAndExpect(
+        'PATCH',
+        '/api/admin/settings/missings-profiles',
+        'clearMissings',
+        5
+      );
     });
   });
 });
