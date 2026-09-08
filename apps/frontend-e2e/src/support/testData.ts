ccd484283606a396523113c012cdf9e8fcb0d8a0
nanoyan <yanpingchenlin@gmail.com>
2026-09-08 15:47:15 +0200

    [e2e] Update to iqb-editor-aspect-3.0.1.html and iqb-player-aspect-3.0.1.html and the imported tests to version 3.0.1

--- apps/frontend-e2e/src/support/testData.ts
+++ apps/frontend-e2e/src/support/testData.ts
@@ -210,9 +210,9 @@ export const resource = 'GeoGebra.itcr.zip';
  */
 export const modules: string[] = [
   'iqb-schemer-2.8.4.html',
-  'iqb-editor-aspect-3.0.0.html',
+  'iqb-editor-aspect-3.0.1.html',
   'iqb-editor-speedtest-3.2.0.html',
-  'iqb-player-aspect-3.0.0.html',
+  'iqb-player-aspect-3.0.1.html',
   'iqb-player-speedtest-3.3.0.html',
   'iqb-player-stars-0.6.26.html'
 ];
