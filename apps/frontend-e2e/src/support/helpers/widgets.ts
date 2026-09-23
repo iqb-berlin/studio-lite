c10b7700f8b91bc76cc559fdeec46dd38fdf1b44
nanoyan <yanpingchenlin@gmail.com>
2026-09-23 18:50:01 +0200

    [e2e] Fix intercept and wait at widget.ts

--- apps/frontend-e2e/src/support/helpers/widgets.ts
+++ apps/frontend-e2e/src/support/helpers/widgets.ts
@@ -14,23 +14,26 @@ export function addWidgets(files: string | string[]) {
 }
 
 export function deleteAllWidgets() {
+  cy.intercept('GET', '**/api/verona-modules*').as('getWidgets');
   cy.visit('/');
   cy.findAdminSettings().click();
   clickIndexTabAdmin('widgets');
 
-  cy.get('body').then($body => {
-    if ($body.find('mat-row').length > 0) {
+  cy.wait('@getWidgets').then(interception => {
+    const widgets = interception.response?.body;
+    if (Array.isArray(widgets) && widgets.length > 0) {
       cy.get('mat-header-cell').find('mat-checkbox').click({ multiple: true });
+      cy.get('[data-cy="delete-widgets-button"]').click();
+      cy.translate(Cypress.expose('locale')).then(json => {
+        cy.clickButtonWithResponseCheck(
+          json.delete,
+          [200],
+          '/api/admin/verona-modules*',
+          'DELETE',
+          'deleteModule'
+        );
+      });
+      cy.get('studio-lite-widgets mat-table').should('not.exist');
     }
-  });
-  cy.get('[data-cy="delete-widgets-button"]').click();
-  cy.translate(Cypress.expose('locale')).then(json => {
-    cy.clickButtonWithResponseCheck(
-      json.delete,
-      [200],
-      '/api/admin/verona-modules*',
-      'DELETE',
-      'deleteModule'
-    );
   });
 }
